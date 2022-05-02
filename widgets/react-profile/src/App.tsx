e38b75b12163d0ca3312e05051258f68535cec92
Abê <agonzalez@modyo.com>
2022-05-02 15:28:27 -0400

    chore: add react-profile widget demo

---
+++ widgets/react-profile/src/App.tsx
@@ -0,0 +1,11 @@
+import { MText, MButton } from '@modyo/react-design-system';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <MText>world!</MText>
+      <MButton text="click me!" />
+    </div>
+  );
+}
