4cccbd4abd5004c1e20cb767ade9b78e28abef11
Abê <agonzalez@modyo.com>
2022-05-09 13:48:25 -0400

    chore: update react instance on react-profile widget example to support multiple widgets instance by site

--- widgets/react-profile/src/index.tsx
+++ widgets/react-profile/src/index.tsx
@@ -5,14 +5,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.querySelectorAll('#react-profile')
+  .forEach((container) => {
+    const root = ReactDOM.createRoot(
+      container as HTMLElement
+    );
+
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
