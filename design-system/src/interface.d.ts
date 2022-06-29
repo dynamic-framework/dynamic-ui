313c7cf8cce89307ef4316f05f6c33dc9976fadf
Eduard Baron <ebaron@modyo.com>
2022-06-29 13:47:04 -0500

    chore: add coupon interface

--- design-system/src/interface.d.ts
+++ design-system/src/interface.d.ts
@@ -2,3 +2,4 @@ export * from './components';
 export * from './index';
 export * from './components/m-alert/m-alert-interface';
 export * from './components/m-button/m-button-interface';
+export * from './components/m-coupon/m-coupon-interface';
