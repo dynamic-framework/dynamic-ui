5e32cad1af2bb500da9fcfcab0dc54110dac18b0
Johann Felipe Rojas <johannrip@gmail.com>
2022-07-27 12:17:35 -0500

    chore: create skeleton component

--- libraries/angular-design-system/src/angular-design-system.module.ts
+++ libraries/angular-design-system/src/angular-design-system.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MSkeleton } from './components/m-skeleton/m-skeleton.component';
+import { ContentLoaderModule } from '@ngneat/content-loader';
 
 import { DIRECTIVES } from './directives/proxies-list';
 
 @NgModule({
-  declarations: [...DIRECTIVES],
-  exports: [...DIRECTIVES],
+  declarations: [...DIRECTIVES, MSkeleton],
+  exports: [...DIRECTIVES, MSkeleton],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
+  imports: [ContentLoaderModule],
 })
 export class AngularDesignSystemModule { }
