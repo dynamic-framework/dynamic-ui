1f61f59fc32919016a2c19016b129cc8e0fb9847
Eduard Baron <ebaron@modyo.com>
2022-08-02 13:36:34 -0500

    chore: add spaces for components and fix m-icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {
   MApp,
   MSelect,
+  MListItem,
+  MButton
 } from '@modyolabs/react-design-system';
 import { useState } from 'react';
 import PaymentPanel from './components/PaymentPanel';
@@ -16,20 +18,50 @@ function App() {
   }])
   return (
     <MApp>
-      <div className="py-4 px-3">
-        <div className='px-4 pb-3'>
+      <div className="container">
+        <div className='pb-3 px-4'>
+          <div className='d-flex justify-content-between align-items-center mb-5 mt-4'>
+            <MButton iconLeft='arrow-left' isPill theme='info' variant='text'></MButton>
+            <h6 className='fw-bold m-0 flex-grow-1 text-center'>Paying Visa ···456</h6>
+          </div>
+
           <MSelect
             mId='selectAccount'
             variant='transparent'
+            theme="info"
           >
             {
-              accounts.map(({id, name}) => (
+              accounts.map(({ id, name }) => (
                 <option key={id} value={id}>From: {name}</option>
               ))
             }
           </MSelect>
+          <br />
+          <MListItem value="12/31/22" text="Pay until" />
+          <MListItem value="$4,956.00" text="To be payed" />
         </div>
-        <PaymentPanel/>
+        <div className='bg-white rounded p-4'>
+          <PaymentPanel />
+          <div className="row g-0 m-0 p-0 mb-5">
+            <div className="col-4">
+              Minimum
+            </div>
+            <div className="col-4">
+              Total
+            </div>
+            <div className="col-4">
+              Payments Alternatives
+            </div>
+            <div className="col-12">
+              More options
+            </div>
+          </div>
+          <div className="row g-0 m-0 p-0">
+            <div className="col-12 d-flex justify-content-center">
+              <MButton text='Pay _/' theme='primary' isPill iconRight='_' iconLeft='_' />
+            </div>
+          </div>
+        </div>
       </div>
     </MApp>
   );
