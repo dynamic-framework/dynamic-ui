1768036a1687ea397c9ef746bafa2dcb073a91cb
Eduard Baron <ebaron@modyo.com>
2022-08-03 15:47:14 -0500

    chore: add handler for amount

--- src/components/PaymentPanel.tsx
+++ src/components/PaymentPanel.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 import {
   MCurrency,
   MButton
 } from '@modyolabs/react-design-system';
-const PaymentPanel = () => {
+const PaymentPanel = ({ base = 1000 }) => {
+  const [amountAvailable, setAmountAvailable] = useState(base);
+  const [amountUsed, setAmountUsed] = useState(undefined);
+  const [theme, setTheme] = useState('info');
+
+  const handlerChange = ({ detail: { amount } }: CustomEvent) => {
+    setAmountUsed(amount);
+  };
+
+  useEffect(() => {
+    if (amountUsed) {
+      if (amountUsed === 0) {
+        setAmountAvailable(base);
+        setTheme('info');
+      } else {
+        setAmountAvailable(base - amountUsed);
+      }
+    }
+  }, [amountUsed]);
+
+  useEffect(() => {
+    if (amountAvailable >= 0) {
+      setTheme('info')
+    } else {
+      setTheme('danger')
+    }
+  }, [amountAvailable]);
+
   return (
     <>
       <div className='p-4 bg-white text-center rounded'>
@@ -11,12 +38,15 @@ const PaymentPanel = () => {
           class='pb-4'
           mId='debtInput'
           placeholder='How much?'
-          theme='info'
-          hint='$ 123,451 remaining'
+          theme={theme}
+          hint={`${amountAvailable}`}
           iconLabel='currency-dollar'
           hintIconStart='info-circle'
-          maxValue={0}
+          minValue={0}
+          maxValue={base}
           variant="prime"
+          onMChange={handlerChange}
+          value={amountUsed}
         />
         <div className="row g-0 m-0 p-0 pt-4 pb-2">
           <div className="col-4">
@@ -33,7 +63,7 @@ const PaymentPanel = () => {
           More options
         </div>
         <div className="d-flex justify-content-center pt-4">
-          <MButton text='Pay _/' theme='primary' isPill iconRight='_' iconLeft='_'/>
+          <MButton text='Pay _/' theme='primary' isPill iconRight='_' iconLeft='_' />
         </div>
       </div>
     </>
