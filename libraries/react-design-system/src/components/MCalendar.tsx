cc73bbaba4b7491be688602fb318f5ade8e3098c
Johann Felipe Rojas <johannrip@gmail.com>
2022-08-03 15:47:53 -0500

    fix: semicolons

--- libraries/react-design-system/src/components/MCalendar.tsx
+++ libraries/react-design-system/src/components/MCalendar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
 export declare interface CalendarProps {
-  date: Date,
-  setDate: Function,
-  calendarContainer?: React.FC
+  date: Date;
+  setDate: Function;
+  calendarContainer?: React.FC;
 };
 
 export default function MCalendar({
