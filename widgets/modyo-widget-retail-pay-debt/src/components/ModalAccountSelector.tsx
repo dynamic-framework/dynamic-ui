97a18879072c83b45400d59b8203d9b3e29c9747
Johann Felipe Rojas <johannrip@gmail.com>
2022-08-05 12:36:41 -0500

    chore: create modals

---
+++ widgets/modyo-widget-retail-pay-debt/src/components/ModalAccountSelector.tsx
@@ -0,0 +1,61 @@
+import { MButton, MListItem, MModal, MQuickAction } from '@modyolabs/react-design-system'
+import React from 'react'
+
+const ModalAccountSelector = () => {
+  return (
+    <MModal
+      mId='accountSelector'
+      centered
+      static
+    >
+      <div
+        slot='body'
+        className='d-flex flex-column justify-content-center align-items-center'>
+        <MListItem
+          variant='selectable'
+          class='w-100'
+          icon=""
+          value="$ 1,234.00"
+          selectableProps={{
+            id: 'account1',
+            name: 'radioAccounts'
+          }}
+          text='{Savings}'
+          subtext='··· 654'
+        />
+        <MListItem
+          variant='selectable'
+          class='w-100'
+          icon=""
+          value="$ 1,234.00"
+          selectableProps={{
+            id: 'account2',
+            name: 'radioAccounts'
+          }}
+          text='{Checking}'
+          subtext='···653'
+        />
+        <MListItem
+          variant='selectable'
+          class='w-100'
+          icon=""
+          value="$ 1,234.00"
+          selectableProps={{
+            id: 'account3',
+            name: 'radioAccounts'
+          }}
+          text='{Current}'
+          subtext='···876'
+        />
+        <MButton
+          data-bs-dismiss="modal"
+          class='my-4'
+          text='Cancel'
+          theme='primary'
+          isPill />
+      </div>
+    </MModal>
+  )
+}
+
+export default ModalAccountSelector
