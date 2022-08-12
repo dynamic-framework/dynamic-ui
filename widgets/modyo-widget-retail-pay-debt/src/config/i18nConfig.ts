b9cadf547d1f6a760b0419cae9dcb32898813e16
Abê <agonzalez@modyo.com>
2022-08-12 14:31:24 -0400

    chore: adjust liquid config and monorepo types

--- widgets/modyo-widget-retail-pay-debt/src/config/i18nConfig.ts
+++ widgets/modyo-widget-retail-pay-debt/src/config/i18nConfig.ts
@@ -1,8 +1,8 @@
+import { liquidParser } from '@modyolabs/design-system';
 import { configureI18n } from '@modyolabs/react-design-system';
 
 import es from '../locales/es.json';
 import en from '../locales/en.json';
-import liquidParser from '../shame/liquidParser';
 
 const resources = {
   es: { translation: es },
@@ -11,8 +11,6 @@ const resources = {
 
 const LANG = liquidParser.parse('{{site.language}}');
 
-// eslint-disable-next-line max-len
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 configureI18n(resources, { lng: LANG })
   .then(console.log)
   .catch(console.error);
