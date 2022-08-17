aa0dd087d3dbf6a545ab94112b95cbf0802af986
Johann Felipe Rojas <johannrip@gmail.com>
2022-08-17 13:30:02 -0500

    chore: export utils on index

--- design-system/src/index.ts
+++ design-system/src/index.ts
@@ -1,3 +1,3 @@
 export { initialize } from './global/initialize';
 export { Config } from './utils/config';
-export { default as liquidParser } from './utils/liquid-parser';
+export * from './utils';
