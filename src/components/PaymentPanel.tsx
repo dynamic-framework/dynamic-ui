1edded10602cf060991cc4e27a43ba80b981bc42
Johann Felipe Rojas <johannrip@gmail.com>
2022-08-23 17:40:47 -0500

    refactor: remove hint and align toggles

--- src/components/PaymentPanel.tsx
+++ src/components/PaymentPanel.tsx
@@ -77,165 +77,162 @@ export default function PaymentPanel() {
 
   return (
     <>
-      <div className="p-4 bg-white text-center rounded">
-        <div className="row g-0 m-0 p-0 pt-4 pb-2">
-          <div className="col-12 flex-column justify-content-between pb-2 mx-auto amount-options">
-            <MShortcutToggle
-              key="1"
-              {...cardToPay.minimumPayment === 0 && { state: 'disabled' }}
-              {...cardToPay.minimumPayment === amount && { isChecked: true }}
-              mId="minimumOption"
-              name="paymentOption"
-              label={t('shortCutToggle.minimum')}
-              text={minimumPayment}
-              value="minimumOption"
-              onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.minimumPayment)}
-            />
-            <MShortcutToggle
-              key="2"
-              {...cardToPay.totalPayment === 0 && { state: 'disabled' }}
-              {...cardToPay.totalPayment === amount && { isChecked: true }}
-              mId="totalOption"
-              name="paymentOption"
-              label={t('shortCutToggle.total')}
-              text={totalPayment}
-              value="totalOption"
-              onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.totalPayment)}
-            />
-            <MShortcutToggle
-              className={
+      <div className="pt-4 pb-2">
+        <div className="d-flex flex-column justify-content-between pb-2 mx-auto amount-options">
+          <p className="px-2 text-gray fw-semibold">Amount</p>
+          <MShortcutToggle
+            class="d-block"
+            key="1"
+            {...cardToPay.minimumPayment === 0 && { state: 'disabled' }}
+            {...cardToPay.minimumPayment === amount && { isChecked: true }}
+            mId="minimumOption"
+            name="paymentOption"
+            label={t('shortCutToggle.minimum')}
+            text={minimumPayment}
+            value="minimumOption"
+            onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.minimumPayment)}
+          />
+          <MShortcutToggle
+            class="d-block"
+            key="2"
+            {...cardToPay.totalPayment === 0 && { state: 'disabled' }}
+            {...cardToPay.totalPayment === amount && { isChecked: true }}
+            mId="totalOption"
+            name="paymentOption"
+            label={t('shortCutToggle.total')}
+            text={totalPayment}
+            value="totalOption"
+            onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.totalPayment)}
+          />
+          <MShortcutToggle
+            className={
                 shortcut !== 'otherAmount' ? 'd-block' : 'd-none'
               }
-              key="3"
-              {...cardToPay.totalPayment === 0 && { state: 'disabled' }}
-              mId="otherAmountOption"
-              name="paymentOption"
-              label={t('shortCutToggle.other')}
-              text={t('shortCutToggle.amount')}
-              value="otherAmount"
-              onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.minimumPayment)}
-            />
-            <MCurrency
-              className={
-                shortcut === 'otherAmount' ? 'd-block' : 'd-none'
+            key="3"
+            {...cardToPay.totalPayment === 0 && { state: 'disabled' }}
+            mId="otherAmountOption"
+            name="paymentOption"
+            label={t('shortCutToggle.other')}
+            text={t('shortCutToggle.amount')}
+            value="otherAmount"
+            onMChange={(e: CustomEvent) => setSelectedOption(e, cardToPay.minimumPayment)}
+          />
+          <MCurrency
+            className={
+                shortcut === 'otherAmount' ? 'd-block mx-2' : 'd-none'
               }
-              mId="debtInput"
-              theme="info"
-              placeholder={t('currencyInput.placeholder')}
-              hint={
-                amountAvailable >= 0
-                  ? t('currencyInput.remainingValid', { remaining: availableFormatted })
-                  : t('currencyInput.remainingInvalid', { amount: format(amount ?? 0) })
-              }
-              iconLabel="currency-dollar"
-              hintIconStart="info-circle"
-              minValue={cardToPay.minimumPayment}
-              maxValue={accountSelected?.value}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-              onMChange={({ detail: { amount: value } }) => setAmount(value)}
-              value={amount}
-            />
-            <MShortcutToggle
-              key="4"
-              mId="alternativeOption"
-              name="paymentOption"
-              label={t('shortCutToggle.paymentAlternatives')}
-              text="..."
-              {...cardToPay.minimumPayment === 0 && { state: 'disabled' }}
-              {...cardToPay.minimumPayment > 0 && {
-                'data-bs-toggle': 'modal',
-                'data-bs-target': '#paymentAlt',
-              }}
-              value="alternativeOption"
-              onMChange={({ detail }: CustomEvent) => setShortcut(detail as string)}
-            />
-          </div>
-        </div>
-        <div className="pb-4">
-          <div
-            className="collapse"
-            id="moreOptions"
-          >
-            <div
-              className="px-3 py-2 border rounded-1 mb-2"
-              {...(cardToPay.minimumPayment > 0 && amount && amountAvailable >= 0) && {
-                'data-bs-toggle': 'modal',
-                'data-bs-target': '#modalSchedulePayment',
-              }}
-            >
-              <MFormSwitch
-                class="d-inline-flex"
-                mId="schedulePayment"
-                label={t('collapse.schedule')}
-                isDisabled
-                isChecked={isScheduled}
-                {...isScheduled && ({ labelOn: t('collapse.yesLabel') })}
-                {...!isScheduled && ({ labelOff: t('collapse.noLabel') })}
-              />
-              <small
-                className="d-block text-info text-start"
-              >
-                {isScheduled
-                  ? t('collapse.yesScheduleLabel', { date: schedule.dateShow })
-                  : t('collapse.noScheduleLabel')}
-              </small>
-            </div>
-            <div
-              className="px-3 py-2 border rounded-1 mb-2"
-              {...(cardToPay.minimumPayment > 0 && amount && amountAvailable >= 0) && {
-                'data-bs-toggle': 'modal',
-                'data-bs-target': '#modalRecurrentPayment',
-              }}
-            >
-              <MFormSwitch
-                class="d-inline-flex"
-                mId="recurrentPayment"
-                label={t('collapse.recurring')}
-                isDisabled
-                isChecked={isRecurrent}
-                {...isRecurrent && ({ labelOn: t('collapse.yesLabel') })}
-                {...!isRecurrent && ({ labelOff: t('collapse.noLabel') })}
-              />
-              <small
-                className="d-block text-info text-start"
-              >
-                {isRecurrent
-                  ? t('collapse.yesRecurrentLabel', { frequency: recurring.start.frequency })
-                  : t('collapse.noRecurrentLabel')}
-              </small>
-            </div>
-          </div>
-          <MButton
-            className="more-options-btn"
-            variant="text"
+            mId="debtInput"
             theme="info"
-            text={t('collapse.options')}
-            iconRight="chevron-down"
-            data-bs-toggle="collapse"
-            data-bs-target="#moreOptions"
-            aria-expanded="false"
-            aria-controls="collapseExample"
-            data-bs-delay="{'show':1000,'hide':1000}"
+            placeholder={t('currencyInput.placeholder')}
+            iconLabel="currency-dollar"
+            hintIconStart="info-circle"
+            minValue={cardToPay.minimumPayment}
+            maxValue={accountSelected?.value}
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            onMChange={({ detail: { amount: value } }) => setAmount(value)}
+            value={amount}
+          />
+          <MShortcutToggle
+            class="d-block"
+            key="4"
+            mId="alternativeOption"
+            name="paymentOption"
+            label={t('shortCutToggle.paymentAlternatives')}
+            text="..."
+            {...cardToPay.minimumPayment === 0 && { state: 'disabled' }}
+            {...cardToPay.minimumPayment > 0 && {
+              'data-bs-toggle': 'modal',
+              'data-bs-target': '#paymentAlt',
+            }}
+            value="alternativeOption"
+            onMChange={({ detail }: CustomEvent) => setShortcut(detail as string)}
+            white
           />
         </div>
+      </div>
+      <div className="pb-4 text-center">
         <div
-          className="d-flex justify-content-center"
+          className="collapse"
+          id="moreOptions"
         >
-          {/* Pointer events?  */}
-          <MButton
-            {...(cardToPay.minimumPayment === 0 || !amount || amountAvailable < 0) && { state: 'disabled' }}
+          <div
+            className="px-3 py-2 border rounded-1 mb-2"
             {...(cardToPay.minimumPayment > 0 && amount && amountAvailable >= 0) && {
               'data-bs-toggle': 'modal',
-              'data-bs-target': '#modalConfirmPayment',
+              'data-bs-target': '#modalSchedulePayment',
             }}
-            text={
+          >
+            <MFormSwitch
+              class="d-inline-flex"
+              mId="schedulePayment"
+              label={t('collapse.schedule')}
+              isDisabled
+              isChecked={isScheduled}
+              {...isScheduled && ({ labelOn: t('collapse.yesLabel') })}
+              {...!isScheduled && ({ labelOff: t('collapse.noLabel') })}
+            />
+            <small
+              className="d-block text-info text-start"
+            >
+              {isScheduled
+                ? t('collapse.yesScheduleLabel', { date: schedule.dateShow })
+                : t('collapse.noScheduleLabel')}
+            </small>
+          </div>
+          <div
+            className="px-3 py-2 border rounded-1 mb-2"
+            {...(cardToPay.minimumPayment > 0 && amount && amountAvailable >= 0) && {
+              'data-bs-toggle': 'modal',
+              'data-bs-target': '#modalRecurrentPayment',
+            }}
+          >
+            <MFormSwitch
+              class="d-inline-flex"
+              mId="recurrentPayment"
+              label={t('collapse.recurring')}
+              isDisabled
+              isChecked={isRecurrent}
+              {...isRecurrent && ({ labelOn: t('collapse.yesLabel') })}
+              {...!isRecurrent && ({ labelOff: t('collapse.noLabel') })}
+            />
+            <small
+              className="d-block text-info text-start"
+            >
+              {isRecurrent
+                ? t('collapse.yesRecurrentLabel', { frequency: recurring.start.frequency })
+                : t('collapse.noRecurrentLabel')}
+            </small>
+          </div>
+        </div>
+        <MButton
+          className="more-options-btn"
+          variant="text"
+          theme="info"
+          text={t('collapse.options')}
+          iconRight="chevron-down"
+          data-bs-toggle="collapse"
+          data-bs-target="#moreOptions"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        />
+      </div>
+      <div
+        className="d-flex justify-content-center"
+      >
+        {/* Pointer events?  */}
+        <MButton
+          {...(cardToPay.minimumPayment === 0 || !amount || amountAvailable < 0) && { state: 'disabled' }}
+          {...(cardToPay.minimumPayment > 0 && amount && amountAvailable >= 0) && {
+            'data-bs-toggle': 'modal',
+            'data-bs-target': '#modalConfirmPayment',
+          }}
+          text={
               isScheduled ? t('button.schedule') : t('button.pay')
             }
-            theme="primary-gradient"
-            isPill
-            iconRight="check-lg"
-          />
-        </div>
+          theme="primary-gradient"
+          isPill
+          iconRight="check-lg"
+        />
       </div>
       <ModalPaymentAlternatives />
       <ModalSchedule onAccept={setIsScheduled} />
