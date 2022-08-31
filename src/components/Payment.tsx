c8a2e0f975286835075cbfcb4be10a9d8ed67965
Eduard Baron <ebaron@modyo.com>
2022-08-31 13:11:35 -0500

    chore: move and change skeleton to Payment.tsx

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -4,6 +4,7 @@ import {
   MIcon,
   MListItem,
   MSelect,
+  MSkeleton,
 } from '@modyolabs/react-design-system';
 import { useTranslation } from 'react-i18next';
 
@@ -54,17 +55,33 @@ export default function Payment() {
               })}
             </h6>
           </div>
-          {accountSelected && (
-            <div className="d-flex flex-column gap-2 bg-light p-3 rounded-1">
-              <MListItem value={dateToPayCard} text={t('nextPayment')} class="p-1" />
-              <MListItem value={totalPayment} text={t('balance')} class="p-1" />
-              { hasMultipleCurrencies && <MListItem value="€1 = USD $1,4" text="Convertion rate" class="p-1" /> }
+
+          {!accountSelected && (
+            <div className="d-flex flex-column justify-content-center align-items-center gap-3 w-100">
+              <MSkeleton viewBox="0 0 320 70" backgroundColor="#e9e9ff" foregroundColor="#f8f8fb">
+                <rect x="0" y="0" rx="8" ry="8" width="320" height="70" />
+              </MSkeleton>
+              <div className="bg-white rounded py-4 w-100">
+                <MSkeleton viewBox="0 0 320 355" backgroundColor="#e9e9ff" foregroundColor="#f8f8fb">
+                  <rect x="0" y="0" rx="8" ry="8" width="320" height="54" />
+                  <rect x="80" y="65" rx="8" ry="8" width="164" height="18" />
+                  <rect x="0" y="120" rx="8" ry="8" width="320" height="54" />
+                  <rect x="0" y="200" rx="8" ry="8" width="320" height="54" />
+                  <rect x="0" y="280" rx="8" ry="8" width="320" height="54" />
+                </MSkeleton>
+              </div>
             </div>
           )}
-          <div className="m-3 pt-3">
-            {accountSelected && (
-              <>
-                { !user.canPayWithoutDebt && (
+
+          {accountSelected && (
+            <>
+              <div className="d-flex flex-column gap-2 bg-light p-3 rounded-1">
+                <MListItem value={dateToPayCard} text={t('nextPayment')} class="p-1" />
+                <MListItem value={totalPayment} text={t('balance')} class="p-1" />
+                {hasMultipleCurrencies && <MListItem value="€1 = USD $1,4" text="Conversion rate" class="p-1" />}
+              </div>
+              <div className="m-3 pt-3">
+                {!user.canPayWithoutDebt && (
                   <div className="d-flex flex-column d-lg-none">
                     <p className="fw-semibold text-info mb-0 px-2 pb-2 text-gray">Pay from</p>
                     <MButton
@@ -96,14 +113,14 @@ export default function Payment() {
                   })}
                   options={accounts}
                   onMChange={
-                    ({ detail: account }:
-                    CustomEvent<Account>) => dispatch(setAccountSelected(account))
+                    // eslint-disable-next-line max-len
+                    ({ detail: account }: CustomEvent<Account>) => dispatch(setAccountSelected(account))
                   }
                 />
-              </>
-            )}
-            <PaymentPanel />
-          </div>
+                <PaymentPanel />
+              </div>
+            </>
+          )}
         </div>
       </div>
       {cardToPay.minimumPayment <= 0 && (
