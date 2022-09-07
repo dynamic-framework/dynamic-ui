abdd342368fdfdd4037a2360a713e3fc9c1c70b3
Eduard Baron <ebaron@modyo.com>
2022-09-07 13:42:32 -0500

    chore: change components and hooks for Schedule

--- widgets/modyo-widget-retail-pay-debt/src/hooks/useSchedule.ts
+++ widgets/modyo-widget-retail-pay-debt/src/hooks/useSchedule.ts
@@ -1,120 +1,85 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useState } from 'react';
 import { useAppDispatch } from '../store/hooks';
+
 import {
-  Day,
   setAutoRepeat,
   setEndRepeat,
   setSchedule,
 } from '../store/slice';
+import useRepeatEnd from './useRepeatEnd';
+import useRepeatStart from './useRepeatStart';
 
 export default function useSchedule(onAccept: (accepted: boolean) => void) {
-  const weekDaysInitial = {
-    saturday: { id: 'saturday', name: 'S', checked: false },
-    monday: { id: 'monday', name: 'M', checked: false },
-    tuesday: { id: 'tuesday', name: 'T', checked: false },
-    wednesday: { id: 'wednesday', name: 'W', checked: false },
-    thursday: { id: 'thursday', name: 'T', checked: false },
-    friday: { id: 'friday', name: 'F', checked: false },
-    sunday: { id: 'sunday', name: 'S', checked: false },
-  };
-
-  const monthlyCycles = [
-    {
-      label: 'Every month.',
-      value: '1-month',
-    },
-    {
-      label: 'Every other month.',
-      value: '2-month',
-    },
-    {
-      label: 'Every 3 months.',
-      value: '3-month',
-    },
-    {
-      label: 'Every 6 months.',
-      value: '6-month',
-    },
-    {
-      label: 'Every 12 months',
-      value: '12-month',
-    },
-  ];
-
   const dispatch = useAppDispatch();
   const [scheduleDay, setScheduleDay] = useState(new Date());
 
-  const [toggleAutoRepeat, setToggleAutoRepeat] = useState(false);
-  const [autoRepeatType, setAutoRepeatType] = useState('weekly');
+  const {
+    toggleStart,
+    setToggleStart,
+    frequencyStart,
+    setFrequencyStart,
+    optionStart,
+    setOptionStart,
+    handleRepeatStart,
+  } = useRepeatStart();
 
-  const [toggleEndRepeat, setToggleEndRepeat] = useState(false);
-  const [endRepeatType, setEndRepeatType] = useState('oneYear');
+  const {
+    toggleEnd,
+    setToggleEnd,
+    frequencyEnd,
+    setFrequencyEnd,
+    optionEnd,
+    setOptionEnd,
+    handleRepeatEnd,
+  } = useRepeatEnd();
 
-  const [weekDays, setWeekDays] = useState<Record<string, Day>>(weekDaysInitial);
-  const [occurrences, setOccurrences] = useState(1);
-
-  const handleAutoRepeatType = ({ detail }: CustomEvent) => setAutoRepeatType(detail as string);
-  const handleEndRepeatType = ({ detail }: CustomEvent) => setEndRepeatType(detail as string);
-
-  function handleWeekDays(day: Day) {
-    setWeekDays((prev) => ({
-      ...prev,
-      [day.id]: {
-        ...day,
-        checked: !day.checked,
-      },
-    }));
-  }
-
-  function handleOccurrences(value: number) {
-    setOccurrences(value);
-  }
-
-  const [date, setDate] = useState(new Date());
-
-  function scheduleDate() {
+  const scheduleDate = () => {
     const newSchedule = {
       isScheduled: true,
-      date: date.toISOString(),
-      dateShow: date.toISOString().split('T')[0], // TODO: add date formatter
+      date: scheduleDay.toISOString(),
+      dateShow: scheduleDay.toISOString().split('T')[0], // TODO: add date formatter
     };
+
     dispatch(setSchedule(newSchedule));
 
-    if (toggleAutoRepeat) {
-      dispatch(setAutoRepeat({
-        enabled: toggleAutoRepeat,
-        frequency: autoRepeatType,
-        option: null,
-      }));
-    }
+    if (newSchedule.isScheduled) {
+      if (toggleStart) {
+        dispatch(setAutoRepeat({
+          enabled: toggleStart,
+          frequency: frequencyStart,
+          option: optionStart,
+        }));
+      }
 
-    if (toggleEndRepeat) {
-      dispatch(setEndRepeat({
-        enabled: toggleEndRepeat,
-        frequency: endRepeatType,
-        option: null,
-      }));
+      if (toggleEnd) {
+        dispatch(setEndRepeat({
+          enabled: toggleEnd,
+          frequency: frequencyEnd,
+          option: optionEnd,
+        }));
+      }
     }
-
     onAccept(true);
-  }
+  };
 
   return {
     scheduleDay,
     setScheduleDay,
-    toggleAutoRepeat,
-    setToggleAutoRepeat,
-    autoRepeatType,
-    handleAutoRepeatType,
-    weekDays,
-    handleWeekDays,
-    monthlyCycles,
-    toggleEndRepeat,
-    setToggleEndRepeat,
-    endRepeatType,
-    handleEndRepeatType,
-    occurrences,
-    handleOccurrences,
     scheduleDate,
+    toggleStart,
+    setToggleStart,
+    frequencyStart,
+    setFrequencyStart,
+    optionStart,
+    setOptionStart,
+    handleRepeatStart,
+    toggleEnd,
+    setToggleEnd,
+    frequencyEnd,
+    setFrequencyEnd,
+    optionEnd,
+    setOptionEnd,
+    handleRepeatEnd,
   };
 }
