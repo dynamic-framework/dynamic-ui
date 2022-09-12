d4a3aa8f58df4dd22ac1d04e87bec30629c7cb18
Eduard Baron <ebaron@modyo.com>
2022-09-12 11:01:13 -0500

    chore: end repeat options refactor

--- src/components/RepeatOptionEnd.tsx
+++ src/components/RepeatOptionEnd.tsx
@@ -1,40 +1,47 @@
 import { MCalendar, MCounter } from '@modyolabs/react-design-system';
+import { Dispatch, SetStateAction } from 'react';
+import { OptionRepeat, OptionRepeatValue } from '../store/slice';
+
+const REPEAT_END_OPTIONS = {
+  oneYear: { id: 'year', name: 'Year' },
+  date: { id: 'date', name: 'Date', value: new Date() },
+  occurrences: { id: 'occurrences', name: 'Occurrences', value: 1 },
+};
 
 interface Props {
   frequency: string;
-  option: { [key: string]: string | boolean | number | Date };
-  setOption: (option: any) => void;
+  option: OptionRepeat;
+  setOption: Dispatch<SetStateAction<OptionRepeat>>;
 }
+
 export default function RepeatOptionEnd({
   frequency,
   option,
   setOption,
 }: Props) {
-  const handleDate = (date: Date) => {
-    setOption((prev: any) => (
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      {
-        ...prev,
-        value: date,
-      }
-    ));
+  const handleDate = (newDate: Date) => {
+    setOption({
+      date: {
+        ...REPEAT_END_OPTIONS.date,
+        value: newDate,
+      },
+    });
   };
 
-  const handleOccurrences = ({ detail }: CustomEvent) => {
-    setOption((prev: any) => (
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      {
-        ...prev,
-        value: detail as number,
-      }
-    ));
+  const handleOccurrences = ({ detail }: CustomEvent<string>) => {
+    setOption({
+      occurrences: {
+        ...REPEAT_END_OPTIONS.occurrences,
+        value: Number.parseInt(detail, 10),
+      },
+    });
   };
 
   return (
     <div className="d-flex w-100">
       {frequency === 'date' && (
         <MCalendar
-          date={option.value as Date}
+          date={option.date?.value as Date ?? REPEAT_END_OPTIONS.date.value}
           setDate={handleDate}
           withPortal
         />
@@ -45,7 +52,7 @@ export default function RepeatOptionEnd({
           mId="counterOccurrences"
           min={1}
           max={100}
-          value={option.value as number}
+          value={option.occurrences?.value as number ?? REPEAT_END_OPTIONS.occurrences.value}
           onMClick={handleOccurrences}
           onMInput={handleOccurrences}
         />
