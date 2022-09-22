d560d9bbcecc54bd8f6623a3adf8ba084f44f6e4
Johann Felipe Rojas <johannrip@gmail.com>
2022-09-22 17:26:24 -0500

    chore: create toast container with preset options

--- libraries/react-design-system/src/components/index.ts
+++ libraries/react-design-system/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './proxies';
 export { default as MCalendar } from './MCalendar';
 export { default as MSkeleton } from './MSkeleton';
+export { default as MToastContainer } from './MToastContainer';
