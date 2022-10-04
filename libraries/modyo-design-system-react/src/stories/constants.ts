8f086e1e7a40ed3781807a076fc196fc24ec91a5
Abê <agonzalez@modyo.com>
2022-10-04 13:23:24 -0300

    chore: add story of m-alert

---
+++ libraries/modyo-design-system-react/src/stories/constants.ts
@@ -0,0 +1,2 @@
+export const THEMES = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark'];
+export const ICONS = ['emoji-smile', 'emoji-smile-upside-down', 'emoji-dizzy', 'emoji-expressionless'];
