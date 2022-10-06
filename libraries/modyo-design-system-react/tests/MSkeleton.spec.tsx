e6af7862da2f8a466f5b26b53686daa8bd409479
Abê <agonzalez@modyo.com>
2022-10-06 18:38:19 -0300

    chore: setup jest on modyo-design-system-react

--- libraries/modyo-design-system-react/tests/MSkeleton.spec.tsx
+++ libraries/modyo-design-system-react/tests/MSkeleton.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
-import MSkeleton from '../src/components/MSkeleton.tsx';
+
+import MSkeleton from '../src/components/MSkeleton';
 
 describe('Base skeleton', () => {
   test('renders without crashing', () => {
     render(
-      <MSkeleton className="hi">
+      <MSkeleton>
         <rect className="hello" x="22" y="13" rx="0" ry="0" width="249" height="51" />
       </MSkeleton>,
     );
