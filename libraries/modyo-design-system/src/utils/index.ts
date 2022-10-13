cd6edb5b4fb1715dff1773c3cfa6efee90199136
Abê <agonzalez@modyo.com>
2022-10-13 19:15:06 -0300

    feat: add utils to mock and make request abortables

--- libraries/modyo-design-system/src/utils/index.ts
+++ libraries/modyo-design-system/src/utils/index.ts
@@ -1,2 +1,5 @@
 export { default as liquidParser } from './liquid-parser';
 export { default as formatCurrency } from './format-currency';
+export { default as createControlledRequest } from './create-controlled-request';
+export { default as createMockedRequest } from './create-mocked-request';
+export { default as Deferred } from './Deferred';
