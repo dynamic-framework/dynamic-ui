ae1617566c25ab0e15d3714dc5ad8392cf6dc39d
Abê <agonzalez@modyo.com>
2022-10-24 12:38:31 -0300

    chore: change signature of request utils

--- libraries/modyo-design-system/src/utils/create-mocked-request.ts
+++ libraries/modyo-design-system/src/utils/create-mocked-request.ts
@@ -1,33 +1,23 @@
-import type {
-  AxiosRequestConfig,
-  AxiosResponse,
-  RawAxiosResponseHeaders,
-} from 'axios';
-
 import Deferred from './Deferred';
 
+/**
+ * @deprecated
+ * @param config
+ * @param data
+ */
 export default function createMockedRequest<T, R = T>(
-  config: AxiosRequestConfig<T>,
   data: R,
 ) {
   const deferred = new Deferred<void>();
   const timer = setTimeout(() => deferred.resolve(), 1000);
   return {
-    abortController: {
-      abort: () => {
-        deferred.reject(new Error('AbortError'));
-        clearTimeout(timer);
-      },
+    abort: () => {
+      deferred.reject(new Error('AbortError'));
+      clearTimeout(timer);
     },
-    perform: async (): Promise<AxiosResponse<R>> => {
+    perform: async (): Promise<R> => {
       await deferred.promise;
-      return {
-        data: data as unknown as R,
-        status: 200,
-        statusText: 'ok',
-        headers: config.headers as RawAxiosResponseHeaders,
-        config,
-      };
+      return data as unknown as R;
     },
   };
 }
