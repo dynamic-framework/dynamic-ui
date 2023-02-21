5cc021622fc3a37936334d4078cb33cac5473ebf
Eduard Baron <ebaron@modyo.com>
2023-02-21 12:28:54 -0500

    feat: add m-collapse component and styles

---
+++ libraries/modyo-design-system-react/src/components/MCollapse.tsx
@@ -0,0 +1,51 @@
+import classNames from 'classnames';
+import { useState, PropsWithChildren, ReactNode } from 'react';
+import { MIcon } from './proxies';
+
+type CollapseProps = PropsWithChildren<{
+  id?: string;
+  Component: JSX.Element | ReactNode;
+  hasSeparator?: boolean;
+}>;
+
+export default function MCollapse({
+  id,
+  Component,
+  hasSeparator = false,
+  children,
+}: CollapseProps) {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <div
+      id={id}
+      className="m-collapse collapse-container"
+    >
+      <div
+        className="collapse-button"
+        role="button"
+        tabIndex={0}
+        onClick={() => setToggle((prev) => !prev)}
+        onKeyDown={({ code }) => (code === 'Enter' ? setToggle((prev) => !prev) : {})}
+      >
+        <div className="flex-grow-1">
+          {Component}
+        </div>
+        <MIcon icon={toggle ? 'chevron-up' : 'chevron-down'} />
+      </div>
+      {toggle && (
+        <div
+          className={classNames({
+            'collapse-body': true,
+            separator: hasSeparator,
+          })}
+        >
+          {hasSeparator && (
+            <div className="collapse-body__separator" />
+          )}
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
