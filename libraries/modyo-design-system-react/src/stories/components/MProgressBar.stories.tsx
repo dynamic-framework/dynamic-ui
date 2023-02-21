3788daf99e1487cbb10870858abcffb9d70ae617
Eduard Baron <ebaron@modyo.com>
2023-02-20 19:44:10 -0500

    chore: add m-progress-bar story

---
+++ libraries/modyo-design-system-react/src/stories/components/MProgressBar.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { MProgressBar } from '../../components';
+
+const config: ComponentMeta<typeof MProgressBar> = {
+  title: 'Stencil/MProgressBar',
+  component: MProgressBar,
+  argTypes: {
+    valueCurrent: {
+      control: 'number',
+    },
+    valueMin: {
+      control: 'number',
+    },
+    valueMax: {
+      control: 'number',
+    },
+    enableStripedAnimation: {
+      control: 'boolean',
+    },
+    enableDarkMode: {
+      control: 'boolean',
+    }
+  },
+};
+
+export default config;
+
+const Template: ComponentStory<typeof MProgressBar> = (args) => (
+  <div className={`d-flex justify-content-center p-3 ${args.enableDarkMode ? 'bg-tertiary' : ''}`}>
+    <MProgressBar {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  valueCurrent: 33,
+  valueMin: 0,
+  valueMax: 100,
+  enableStripedAnimation: false,
+  enableDarkMode: false,
+};
