6aaf2618e741134ae3cad3cb90935c3056c23592
Eduard Baron <ebaron@modyo.com>
2023-03-02 10:49:26 -0500

    chore: add stories

---
+++ libraries/modyo-design-system-react/src/stories/components/MPin.stories.tsx
@@ -0,0 +1,66 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { MPin } from '../../components';
+
+const config: ComponentMeta<typeof MPin> = {
+  title: 'Stencil/Pin',
+  component: MPin,
+  argTypes: {
+    characters: {
+      control: 'number',
+    },
+    theme: {
+      control: 'select',
+      options: [undefined, 'tertiary', 'danger', 'warning'],
+    },
+    mType: {
+      control: 'select',
+      options: ['number', 'text'],
+    },
+    isDisabled: {
+      control: 'boolean',
+    },
+    isLoading: {
+      control: 'boolean',
+    },
+    label: {
+      control: 'text',
+    },
+    labelIcon: {
+      control: 'text',
+    },
+    iconStart: {
+      control: 'text',
+    },
+    iconEnd: {
+      control: 'text',
+    },
+    hint: {
+      control: 'text',
+    },
+    hintIconStart: {
+      control: 'text',
+    },
+    hintIconEnd: {
+      control: 'text',
+    },
+  },
+};
+
+export default config;
+
+const Template: ComponentStory<typeof MPin> = (args) => (
+ <div className="d-flex">
+   <MPin {...args} />
+ </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  mId: 'myPin',
+  characters: 4,
+  theme: undefined,
+  mType: 'text',
+  isDisabled: false,
+  isLoading: false,
+};
