3fe8eb41368df9921740e5241f9391431a3c5a95
Eduard Baron <ebaron@modyo.com>
2023-03-06 08:51:49 -0500

    chore: adjust test

--- libraries/modyo-design-system/src/components/m-pin/m-pin.spec.tsx
+++ libraries/modyo-design-system/src/components/m-pin/m-pin.spec.tsx
@@ -16,11 +16,11 @@ it('should render my component', async () => {
   });
   expect(page.root).toEqualHtml(`
   <m-pin class="form-control-layout form-control-layout-pin">
-    <form class="m-pin" id="${pin.mId}">
-      <input autocomplete="off" class="m-pin-item" id="pin-index-0" inputmode="text" maxlength="1" name="pin-0" placeholder="•" required type="text">
-      <input autocomplete="off" class="m-pin-item" id="pin-index-1" inputmode="text" maxlength="1" name="pin-1" placeholder="•" required type="text">
-      <input autocomplete="off" class="m-pin-item" id="pin-index-2" inputmode="text" maxlength="1" name="pin-2" placeholder="•" required type="text">
-      <input autocomplete="off" class="m-pin-item" id="pin-index-3" inputmode="text" maxlength="1" name="pin-3" placeholder="•" required type="text">
+    <form class="form-control-input" id="${pin.mId}">
+      <input autocomplete="off" class="pin-item" id="pin-index-0" inputmode="text" maxlength="1" name="pin-0" placeholder="•" required type="text">
+      <input autocomplete="off" class="pin-item" id="pin-index-1" inputmode="text" maxlength="1" name="pin-1" placeholder="•" required type="text">
+      <input autocomplete="off" class="pin-item" id="pin-index-2" inputmode="text" maxlength="1" name="pin-2" placeholder="•" required type="text">
+      <input autocomplete="off" class="pin-item" id="pin-index-3" inputmode="text" maxlength="1" name="pin-3" placeholder="•" required type="text">
     </form>
   </m-pin>
   `);
