b61bd697b0763007e38b92e476699e659f70071e
Abê <agonzalez@modyo.com>
2023-05-26 19:07:54 -0400

    test: adjust m-quick-action-button

--- libraries/modyo-design-system/src/components/m-quick-action-button/m-quick-action-button.spec.tsx
+++ libraries/modyo-design-system/src/components/m-quick-action-button/m-quick-action-button.spec.tsx
@@ -20,19 +20,19 @@ it('should render my component', async () => {
   });
   expect(page.root).toEqualHtml(`
     <m-quick-action-button>
-      <button class="quick-action">
-        <img alt="" class="quick-action-picture" src="https://i.pravatar.cc/150?img=2">
-        <div class="quick-action-content">
-          <div class="quick-action-text">
-            <span class="quick-action-title">
+      <button class="quick-action-button">
+        <img alt="" class="quick-action-button-representative-image" src="https://i.pravatar.cc/150?img=2">
+        <div class="quick-action-button-content">
+          <div class="quick-action-button-text">
+            <span class="quick-action-button-line1">
               Jessica Rabit
             </span>
-            <small class="quick-action-subtitle">
+            <small class="quick-action-button-line2">
               Toon Bank **** 721
             </small>
           </div>
         </div>
-        <m-icon class="quick-action-action-icon" icon="chevron-right"></m-icon>
+        <m-icon class="quick-action-button-action-icon" icon="chevron-right" size="var(--bs-m-quick-action-button-action-icon-size)"></m-icon>
       </button>
     </m-quick-action-button>
   `);
