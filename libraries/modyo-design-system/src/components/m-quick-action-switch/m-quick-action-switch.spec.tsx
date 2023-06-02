7d39b4694748242f48dcdaa4f514eee4a46cc054
Abê <agonzalez@modyo.com>
2023-06-02 11:41:15 -0400

    test: adjust test of m-quick-action-switch

--- libraries/modyo-design-system/src/components/m-quick-action-switch/m-quick-action-switch.spec.tsx
+++ libraries/modyo-design-system/src/components/m-quick-action-switch/m-quick-action-switch.spec.tsx
@@ -7,8 +7,8 @@ it('should render my component', async () => {
   const shortcut = {
     mId: 'component',
     name: 'name',
-    line1: 'total',
-    line2: '$ 7.432.450',
+    label: 'total',
+    hint: '$ 7.432.450',
     value: 'value',
   };
 
@@ -21,13 +21,17 @@ it('should render my component', async () => {
     ),
   });
   expect(page.root).toEqualHtml(`
-    <m-quick-action-switch line1="total" line2="$ 7.432.450" value="value">
+    <m-quick-action-switch value="value">
       <label class="m-quick-action-switch" htmlfor="component">
         <div class="m-quick-action-switch-content">
           <m-input-switch mid="component" name="name"></m-input-switch>
-          <span class="m-quick-action-switch-label"></span>
+          <span class="m-quick-action-switch-label">
+            total
+          </span>
         </div>
-        <div class="m-quick-action-switch-hint"></div>
+        <div class="m-quick-action-switch-hint">
+          $ 7.432.450
+        </div>
       </label>
     </m-quick-action-switch>
   `);
