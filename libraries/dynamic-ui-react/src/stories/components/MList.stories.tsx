85dbe338b2cb5965bb925a84011eef743da7747b
Eduard Baron <ebaron@modyo.com>
2023-07-28 15:38:38 -0500

    chore: adjust stories and rename file

--- libraries/dynamic-ui-react/src/stories/components/MList.stories.tsx
+++ libraries/dynamic-ui-react/src/stories/components/MList.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ComponentProps } from 'react';
 import { MList, MListItem } from '../../components';
-import MListItemMovement from '../../components/MListItemMovement';
 
 const config: Meta<typeof MList> = {
   title: 'Design System/Alpha/Patterns/List',
@@ -39,21 +38,6 @@ const MListExample = (args: ComponentProps<typeof MList>) => (
       >
         Lorem ipsum dolor sit amet consectetur.
       </MListItem>
-    ))}
-  </MList>
-);
-
-const MListExampleMovements = (args: ComponentProps<typeof MList>) => (
-  <MList {...args}>
-    {[1, 2, 3, 4, 5].map((item) => (
-      <MListItemMovement
-        key={item}
-        line1={`Credit Card ${item}`}
-        line2="22/05/23"
-        line3={item % 2 === 1 ? 5000 : -5000}
-      >
-        Lorem ipsum dolor sit amet consectetur.
-      </MListItemMovement>
     ))}
   </MList>
 );
@@ -111,22 +95,5 @@ export const HorizontalWithBreakpoint: Story = {
     isHorizontal: true,
     horizontalBreakpoint: 'md',
   },
-  storyName: 'Horizontal with breakpoint',
-};
-
-export const CustomList: Story = {
-  decorators: [
-    (Story) => (
-      <div style={{ width: 'calc(300px - 1rem)' }} className="position-relative">
-        <Story />
-      </div>
-    ),
-  ],
-  render: (args) => (
-    <MListExampleMovements {...args} />
-  ),
-  args: {
-    isFlush: true,
-  },
-  name: 'Custom list - Movements',
+  name: 'Horizontal with breakpoint',
 };
