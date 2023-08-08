5323d8a6556bc30765d2474518f75177dffcf09e
Eduard Baron <ebaron@modyo.com>
2023-08-08 13:25:37 -0500

    chore: adjust story

--- libraries/dynamic-ui-react/src/stories/components/MPaginator.stories.tsx
+++ libraries/dynamic-ui-react/src/stories/components/MPaginator.stories.tsx
@@ -13,7 +13,7 @@ const meta: Meta<typeof MPaginator> = {
       control: 'number',
     },
     onPageChange: {
-      control: 'clicked',
+      type: 'function',
     },
     previousLabel: {
       control: 'text',
