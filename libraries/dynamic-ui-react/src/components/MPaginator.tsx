6f10356217df8438b6087453d11b505af5845fee
Eduard Baron <ebaron@modyo.com>
2023-08-08 11:05:34 -0500

    chore: add m-paginator component

--- libraries/dynamic-ui-react/src/components/MPaginator.tsx
+++ libraries/dynamic-ui-react/src/components/MPaginator.tsx
@@ -1,102 +1,40 @@
 import classNames from 'classnames';
-import {
-  useMemo,
-} from 'react';
-import { MButton } from './proxies';
-import usePagination, { DOTS } from '../hooks/usePagination';
+import ResponsivePagination, { ResponsivePaginationProps } from 'react-responsive-pagination';
 
-type Props = {
-  totalCount: number;
-  pageSize: number;
-  siblingCount?: number;
-  prevText?: string;
-  nextText?: string;
-  currentPage: number;
-  setCurrentPage: (currentPage: number) => void;
+export type MPaginatorProps = {
+  extraClassName?: ResponsivePaginationProps['extraClassName'];
+  current: ResponsivePaginationProps['current'];
+  onPageChange: ResponsivePaginationProps['onPageChange'];
+  total: ResponsivePaginationProps['total'];
+  previousLabel?: ResponsivePaginationProps['previousLabel'];
+  nextLabel?: ResponsivePaginationProps['nextLabel'];
+  showArrows?: ResponsivePaginationProps['renderNav'];
+  maxWidth?: ResponsivePaginationProps['maxWidth'];
 };
-
-export default function MPaginator(
-  {
-    totalCount,
-    setCurrentPage,
-    currentPage,
-    siblingCount = 1,
-    pageSize,
-    prevText,
-    nextText,
-  }: Props,
-) {
-  const pages = usePagination({
-    currentPage, siblingCount, totalCount, pageSize,
-  });
-
-  const actions = useMemo(() => ({
-    prevPage: () => setCurrentPage(currentPage - 1),
-    nextPage: () => setCurrentPage(currentPage + 1),
-    setPage: (page: number) => setCurrentPage(page),
-  }), [currentPage, setCurrentPage]);
-
-  const isDisabled = useMemo(() => ({
-    prev: currentPage === 1,
-    next: currentPage === pages[pages.length - 1],
-  }), [currentPage, pages]);
-
-  if (currentPage === 0 || pages.length < 2) {
-    return null;
-  }
-
+export default function MPaginator({
+  extraClassName,
+  nextLabel,
+  previousLabel,
+  showArrows = true,
+  ...props
+}:
+MPaginatorProps) {
   return (
-    <div className="m-paginator">
-      <div className="m-paginator-arrow">
-        <MButton
-          isPill
-          iconStart="chevron-left"
-          theme="secondary"
-          variant="link"
-          onMClick={actions.prevPage}
-          text={prevText}
-          size="sm"
-          isDisabled={isDisabled.prev}
-        />
-      </div>
-      <div className="m-paginator-pages">
-        {pages.map((pageItem, index) => {
-          if (typeof pageItem === 'string') {
-            return (
-              <span
-                // eslint-disable-next-line react/no-array-index-key
-                key={`Dot-${index}`}
-                className="m-paginator-page-dots"
-              >
-                {DOTS}
-              </span>
-            );
-          }
-
-          return (
-            <button
-              key={pageItem}
-              type="button"
-              onClick={() => actions.setPage(pageItem)}
-              className={classNames(currentPage === pageItem ? 'active' : '', 'm-paginator-page')}
-            >
-              {pageItem}
-            </button>
-          );
-        })}
-      </div>
-      <div className="m-paginator-arrow">
-        <MButton
-          isPill
-          iconEnd="chevron-right"
-          theme="secondary"
-          variant="link"
-          size="sm"
-          onMClick={actions.nextPage}
-          text={nextText}
-          isDisabled={isDisabled.next}
-        />
-      </div>
-    </div>
+    <ResponsivePagination
+      extraClassName={classNames('m-pagination', extraClassName)}
+      nextClassName={classNames(
+        'm-arrow m-arrow-next',
+        !nextLabel && 'no-label',
+      )}
+      nextLabel={nextLabel}
+      previousClassName={classNames(
+        'm-arrow m-arrow-prev',
+        !previousLabel && 'no-label',
+      )}
+      previousLabel={previousLabel}
+      renderNav={showArrows}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
   );
 }
