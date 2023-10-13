7f1f155a639f40f54411958f0171d823b4c21ecd
Abê <agonzalez@modyo.com>
2023-10-13 11:28:12 -0300

    feat: move styles and remove angular, vue and stencil packages

--- libraries/dynamic-ui/scripts/custom-elements/custom-elements.d.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-export * from './index';
-// @ts-ignore
-export * from '../dist/types/interface';
--- libraries/dynamic-ui/src/utils/format-currency.ts
+++
@@ -1,9 +0,0 @@
-import currency from 'currency.js';
-import type { Options } from 'currency.js';
-
-export default function formatCurrency(
-  amount: number,
-  options: Options,
-): string {
-  return currency(amount, options).format();
-}
