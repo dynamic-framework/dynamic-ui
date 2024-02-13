28e8280cddfd3330fc2a06aebd433f201e56a12a
Abê <agonzalez@modyo.com>
2024-02-13 19:28:28 -0300

    feat: use close icon on offcanvas from DContext

--- libraries/dynamic-ui-react/src/components/DOffcanvas/components/DOffcanvasHeader.tsx
+++ libraries/dynamic-ui-react/src/components/DOffcanvas/components/DOffcanvasHeader.tsx
@@ -1,9 +1,10 @@
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useMemo } from 'react';
 
 import classNames from 'classnames';
 import DIcon from '../../DIcon';
 
 import type { BaseProps, FamilyIconProps } from '../../interface';
+import { useDContext } from '../../../contexts';
 
 type Props =
 & BaseProps
@@ -24,10 +25,17 @@ export default function DOffcanvasHeader(
     style,
     iconFamilyClass,
     iconFamilyPrefix,
-    icon = 'x-lg',
+    icon: iconProp,
     materialStyle = false,
   }: Props,
 ) {
+  const {
+    iconMap: {
+      xLgIcon,
+    },
+  } = useDContext();
+  const icon = useMemo(() => iconProp || xLgIcon, [iconProp, xLgIcon]);
+
   return (
     <div
       className={classNames('offcanvas-header', className)}
--- libraries/dynamic-ui-react/stories/components/DOffcanvas.stories.tsx
+++ libraries/dynamic-ui-react/stories/components/DOffcanvas.stories.tsx
@@ -356,11 +356,29 @@ export const MaterialStyleCloseIcon: Story = {
         xIcon: 'close_small',
         xLgIcon: 'close',
         chevronDownIcon: 'expand_more',
+        alert: {
+          warning: 'warning',
+          danger: 'error',
+          success: 'done',
+          info: 'info',
+          dark: 'info',
+          light: 'info',
+          primary: 'info',
+          secondary: 'info',
+        },
+        input: {
+          invalid: 'priority_high',
+          valid: 'done',
+          search: 'search',
+          show: 'visibility',
+          hide: 'visibility_off',
+          increase: 'add_box',
+          decrease: 'indeterminate_check_box',
+        },
       }}
     >
       <DOffcanvas {...args}>
         <DOffcanvas.Header
-          icon="cancel"
           showCloseButton
         >
           <h5 className="fw-bold">Advanced filters</h5>
