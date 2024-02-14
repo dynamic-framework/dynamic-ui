0ec983ab4539ffbf4ef11de701bb1a5f7252ad02
Johann Felipe Rojas <johannrip@gmail.com>
2024-02-14 17:18:18 -0500

    chore: update toast example

--- libraries/dynamic-ui-react/stories/hooks/useToastExamples.tsx
+++ libraries/dynamic-ui-react/stories/hooks/useToastExamples.tsx
@@ -2,6 +2,7 @@ import useToast from '../../src/components/DToastContainer/useToast';
 import DButton from '../../src/components/DButton';
 import DToastContainer from '../../src/components/DToastContainer';
 import { DContextProvider } from '../../src';
+import { DContextProviderConfigMaterial } from '../config/contextProviderMaterial';
 
 export function UseToastExample() {
   const { toast } = useToast();
@@ -25,36 +26,7 @@ export function UseToastMaterialExample() {
   const { toast } = useToast();
   return (
     <DContextProvider
-      icon={{
-        materialStyle: true,
-        familyClass: 'material-symbols-outlined',
-        familyPrefix: '',
-      }}
-      iconMap={{
-        x: 'close',
-        xLg: 'close',
-        chevronDown: 'expand_more',
-        upload: 'cloud_upload',
-        alert: {
-          warning: 'warning',
-          danger: 'error',
-          success: 'done',
-          info: 'info',
-          dark: 'info',
-          light: 'info',
-          primary: 'info',
-          secondary: 'info',
-        },
-        input: {
-          invalid: 'priority_high',
-          valid: 'done',
-          search: 'search',
-          show: 'visibility',
-          hide: 'visibility_off',
-          increase: 'add_box',
-          decrease: 'indeterminate_check_box',
-        },
-      }}
+      {...DContextProviderConfigMaterial}
     >
       <DButton
         text="Show Toast"
