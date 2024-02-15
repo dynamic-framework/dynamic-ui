a610603c910c1f220d8b8326c9d0795b57550042
Johann Felipe Rojas <johannrip@gmail.com>
2024-02-15 10:57:46 -0500

    refactor: remove default prop value

--- libraries/dynamic-ui-react/src/components/DStepper/DStepper.tsx
+++ libraries/dynamic-ui-react/src/components/DStepper/DStepper.tsx
@@ -24,7 +24,7 @@ export default function DStepper(
   {
     options,
     currentStep,
-    iconSuccess = 'check',
+    iconSuccess,
     iconSuccessFamilyClass,
     iconSuccessFamilyPrefix,
     iconSuccessMaterialStyle = false,
