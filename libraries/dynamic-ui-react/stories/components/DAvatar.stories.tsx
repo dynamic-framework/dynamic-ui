cf5ec685f18038a804de5622d03270f2e8c234f1
Abe <agonzalez@modyo.com>
2024-03-25 15:45:18 -0300

    feat: add avatar-group example and fix DContext story

--- libraries/dynamic-ui-react/stories/components/DAvatar.stories.tsx
+++ libraries/dynamic-ui-react/stories/components/DAvatar.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import type { ComponentProps } from 'react';
 import DAvatar from '../../src/components/DAvatar/DAvatar';
 import { AVATAR_SIZE } from '../config/constants';
 import { PREFIX_BS } from '../../src/components/config';
@@ -77,5 +78,26 @@ export const Large: Story = {
   args: {
     size: 'xxl',
     image: 'https://cdn.modyo.cloud/uploads/03a6970d-e917-4597-8c9f-bae052a214ab/original/Avatars_1_.png',
+  },
+};
+
+export const Group: Story = {
+  render: (args: ComponentProps<typeof DAvatar>) => (
+    <div className="avatar-group">
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+    </div>
+  ),
+  args: {
+    image: 'https://cdn.modyo.cloud/uploads/03a6970d-e917-4597-8c9f-bae052a214ab/original/Avatars_1_.png',
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown',
+      },
+    },
   },
 };
