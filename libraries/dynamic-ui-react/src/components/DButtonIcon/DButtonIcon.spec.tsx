c61389c93c7339b695f5dcdf5c144e065cf19865
Johann F Rojas <43022731+johannrp27@users.noreply.github.com>
2024-06-27 15:01:25 -0500

    feat(DIconButton): create component, test and stories (#532)
    Co-authored-by: Eduard Baron <42986303+efbarong@users.noreply.github.com>

---
+++ libraries/dynamic-ui-react/src/components/DButtonIcon/DButtonIcon.spec.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import DButtonIcon from './DButtonIcon';
+
+it('should render base icon button', () => {
+  const props = { icon: 'x-lg' };
+
+  const { container } = render(
+    <DButtonIcon {...props} />,
+  );
+
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <button
+        class="btn d-button-icon btn-primary"
+        type="button"
+      >
+        <i
+          class="d-icon bi bi-x-lg"
+          style="--bs-icon-component-size: 1.5rem; --bs-icon-component-loading-duration: 1.8s; --bs-icon-component-padding: 0;"
+        />
+      </button>
+    </div>
+  `);
+});
