ed3291d6cc5f0848fe755fc80323029d82c8da4e
Johann Felipe Rojas <johannrip@gmail.com>
2024-07-23 14:16:34 -0500

    feat(docs): add dynamic framework ui logo

--- libraries/dynamic-ui-react/.storybook/theme.ts
+++ libraries/dynamic-ui-react/.storybook/theme.ts
@@ -6,7 +6,7 @@ export default create({
 
   brandTitle: 'Dynamic',
   brandUrl: 'https://react.dynamicframework.dev',
-  brandImage: 'https://cloud.modyocdn.com/uploads/77647270-2c05-40b4-9fa1-2e57fba27f80/original/dynamic_framework.svg',
+  brandImage: 'https://cdn.modyo.cloud/uploads/8c051a86-0d5b-4064-b5fd-76fb346e0fb0/original/Dynamic_Framework_UI.svg',
   brandTarget: '_self',
 
   colorPrimary: '#d81b60',
