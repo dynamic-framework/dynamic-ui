74cd2ab57417221dc1fa133f496c4689bb1f696e
Abê <102051289+abe-modyo@users.noreply.github.com>
2024-08-27 11:49:27 -0400

    chore: deploy to s3 cdn (#720)

---
+++ libraries/dynamic-ui-react/scripts/publish-cdn.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { execSync } = require('node:child_process');
+
+const packageDir = path.resolve(__dirname, '..')
+
+const packageJsonPath = path.resolve(packageDir, 'package.json');
+const packageJson = require(packageJsonPath)
+const version = packageJson.version;
+
+const bucketPath = `s3://dynamicframework-cdn/assets/${version}/ui-react`;
+
+try {
+  execSync(`aws s3 sync ./dist/ ${bucketPath} --delete`, { stdio: 'inherit' });
+  console.log(`Sync completed successfully. Version: ${version}`);
+} catch (error) {
+  console.error('Error during sync:', error);
+  process.exit(1);
+}
