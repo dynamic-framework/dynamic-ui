e9258679065633fb63a06b6d1060fc9e415df05d
Abe <agonzalez@modyo.com>
2024-08-28 12:55:14 -0400

    fix: add acl to s3 sync and adjust on tag regex on action

--- libraries/dynamic-ui-react/scripts/publish-cdn.js
+++ libraries/dynamic-ui-react/scripts/publish-cdn.js
@@ -1,17 +1,17 @@
-const fs = require('node:fs');
 const path = require('node:path');
 const { execSync } = require('node:child_process');
 
-const packageDir = path.resolve(__dirname, '..')
+const packageDir = path.resolve(__dirname, '..');
 
 const packageJsonPath = path.resolve(packageDir, 'package.json');
-const packageJson = require(packageJsonPath)
-const version = packageJson.version;
+// eslint-disable-next-line import/no-dynamic-require
+const packageJson = require(packageJsonPath);
+const { version } = packageJson;
 
 const bucketPath = `s3://dynamicframework-cdn/assets/${version}/ui-react`;
 
 try {
-  execSync(`aws s3 sync ./dist/ ${bucketPath} --delete`, { stdio: 'inherit' });
+  execSync(`aws s3 sync ./dist/ ${bucketPath} --delete --acl public-read`, { stdio: 'inherit' });
   console.log(`Sync completed successfully. Version: ${version}`);
 } catch (error) {
   console.error('Error during sync:', error);
