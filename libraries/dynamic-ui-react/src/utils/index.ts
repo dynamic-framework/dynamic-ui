c722ed5d57b1edd202103e3e7d125e7782f636fe
Abe <agonzalez@modyo.com>
2024-10-14 20:33:51 -0300

    feat: add query string utilities

--- libraries/dynamic-ui-react/src/utils/index.ts
+++ libraries/dynamic-ui-react/src/utils/index.ts
@@ -1,2 +1,4 @@
 export { default as formatCurrency } from './formatCurrency';
 export { default as configureI18n } from './configureI18n';
+export { default as getQueryString } from './getQueryString';
+export { default as changeQueryString } from './changeQueryString';
