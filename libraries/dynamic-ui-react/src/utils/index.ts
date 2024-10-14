7606260a7db1aae5cf0e46fe8019b7c8f1a59b0a
Abe <agonzalez@modyo.com>
2024-10-14 20:31:31 -0300

    chore: rename formatCurrency file following the naming convention

--- libraries/dynamic-ui-react/src/utils/index.ts
+++ libraries/dynamic-ui-react/src/utils/index.ts
@@ -1,2 +1,2 @@
-export { default as formatCurrency } from './format-currency';
+export { default as formatCurrency } from './formatCurrency';
 export { default as configureI18n } from './configureI18n';
