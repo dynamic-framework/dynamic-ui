2fd441724aa138d87b9b1c275b4e7254e1f5fee7
abe-modyo <102051289+abe-modyo@users.noreply.github.com>
2024-11-28 22:10:16 +0000

    doc: update storybook

---
+++ docs/assets/DAvatar.stories-CvIiN1ch.js
@@ -0,0 +1,56 @@
+import{j as e}from"./jsx-runtime-DQ32znRX.js";import{r as A}from"./index-DH5ua8nC.js";import{c as L}from"./index-mLFX9G4A.js";import{A as O,C as G}from"./constants-DhWTmuJQ.js";import{P as r}from"./config-CUZHf55T.js";import"./_commonjsHelpers-Cpj98o6Y.js";function s({id:a,size:c,image:m,name:t,useNameAsInitials:f=!1,theme:p="secondary",variant:g,className:M,style:R,dataAttributes:k}){const J=A.useMemo(()=>({"d-avatar":!0,[g?`d-avatar-${g}-${p}`:`d-avatar-${p}`]:!0,[`d-avatar-${c}`]:!!c}),[g,p,c]),v=A.useMemo(()=>{if(t)return f?t:t.split(/\s+/).map(y=>y.charAt(0)).join("").toUpperCase()},[t,f]);return e.jsxs("div",{className:L(J,M),style:R,id:a,...k,children:[m&&e.jsx("img",{src:m,alt:t,className:"d-avatar-img"}),v&&!m&&e.jsx("span",{className:"d-avatar-name",children:v})]})}try{s.displayName="DAvatar",s.__docgenInfo={description:"",displayName:"DAvatar",props:{style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties | undefined"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string | undefined"}},dataAttributes:{defaultValue:null,description:"",name:"dataAttributes",required:!1,type:{name:"DataAttributes | undefined"}},id:{defaultValue:null,description:"",name:"id",required:!1,type:{name:"string | undefined"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"sm"'},{value:'"lg"'},{value:'"xs"'},{value:'"xl"'},{value:'"xxl"'}]}},image:{defaultValue:null,description:"",name:"image",required:!1,type:{name:"string | undefined"}},name:{defaultValue:null,description:"",name:"name",required:!1,type:{name:"string | undefined"}},useNameAsInitials:{defaultValue:{value:"false"},description:"",name:"useNameAsInitials",required:!1,type:{name:"boolean | undefined"}},theme:{defaultValue:{value:"secondary"},description:"",name:"theme",required:!1,type:{name:"string | undefined"}},variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"light"'},{value:'"dark"'}]}}}}}catch{}const P={title:"Design System/Components/Avatar",component:s,parameters:{docs:{description:{component:`
+## CSS Variables
+| Variable                               | Class   | Type            | Description             |
+| -------------------------------------- | ------- | --------------- | ----------------------- |
+| --${r}avatar-border            | .avatar | css border      | Border box              |
+| --${r}avatar-size              | .avatar | css length unit | Size                    |
+| --${r}avatar-bg                | .avatar | css color unit  | Text background         |
+| --${r}avatar-color             | .avatar | css color unit  | Text color              |
+| --${r}avatar-font-weight       | .avatar | css font weight | Text font weight        |
+| --${r}avatar-border-radius     | .avatar | css length unit | Border radius           |
+        `}}},argTypes:{id:{control:"text",type:"string"},className:{control:"text",type:"string"},style:{control:"object"},size:{control:{type:"select",labels:{undefined:"empty"}},type:"string",options:[void 0,...O],description:"Size"},image:{control:"text",type:"string",description:"URL of the avatar image"},name:{control:"text",type:"string",description:"The text to display"},useNameAsInitials:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}},description:"Take the name as name initials"},theme:{control:"select",type:"string",options:G,table:{defaultValue:{summary:"secondary"}},description:"Theme"},variant:{control:{type:"radio",labels:{undefined:"default"}},type:"string",options:[void 0,"light","dark"],description:"Variant"}},tags:["autodocs"]},n={args:{name:"John Doe"}},o={args:{size:"sm",name:"AB",useNameAsInitials:!0}},i={args:{size:"lg",name:"AB",useNameAsInitials:!0}},l={args:{size:"xxl",name:"AB",useNameAsInitials:!0}},d={render:a=>e.jsxs("div",{className:"d-avatar-group",children:[e.jsx(s,{...a}),e.jsx(s,{...a}),e.jsx(s,{...a}),e.jsx(s,{...a})]}),args:{name:"AB",useNameAsInitials:!0},parameters:{docs:{canvas:{sourceState:"shown"}}}},u={args:{image:"https://cdn.modyo.cloud/uploads/03a6970d-e917-4597-8c9f-bae052a214ab/original/Avatars_1_.png",name:"John Doe"}};var x,h,b;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    name: 'John Doe'
+  }
+}`,...(b=(h=n.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var N,S,D;o.parameters={...o.parameters,docs:{...(N=o.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    size: 'sm',
+    name: 'AB',
+    useNameAsInitials: true
+  }
+}`,...(D=(S=o.parameters)==null?void 0:S.docs)==null?void 0:D.source}}};var _,V,I;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    size: 'lg',
+    name: 'AB',
+    useNameAsInitials: true
+  }
+}`,...(I=(V=i.parameters)==null?void 0:V.docs)==null?void 0:I.source}}};var j,B,C;l.parameters={...l.parameters,docs:{...(j=l.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    size: 'xxl',
+    name: 'AB',
+    useNameAsInitials: true
+  }
+}`,...(C=(B=l.parameters)==null?void 0:B.docs)==null?void 0:C.source}}};var q,z,T;d.parameters={...d.parameters,docs:{...(q=d.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: (args: ComponentProps<typeof DAvatar>) => <div className="d-avatar-group">
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+      <DAvatar {...args} />
+    </div>,
+  args: {
+    name: 'AB',
+    useNameAsInitials: true
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown'
+      }
+    }
+  }
+}`,...(T=(z=d.parameters)==null?void 0:z.docs)==null?void 0:T.source}}};var $,E,w;u.parameters={...u.parameters,docs:{...($=u.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    image: 'https://cdn.modyo.cloud/uploads/03a6970d-e917-4597-8c9f-bae052a214ab/original/Avatars_1_.png',
+    name: 'John Doe'
+  }
+}`,...(w=(E=u.parameters)==null?void 0:E.docs)==null?void 0:w.source}}};const Q=["Default","Small","Medium","Large","Group","Image"];export{n as Default,d as Group,u as Image,l as Large,i as Medium,o as Small,Q as __namedExportsOrder,P as default};
---
+++ docs/assets/DInputMask-D85JFeo0.js
@@ -0,0 +1,68 @@
+import{j as t}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as l}from"./index-COxJNofV.js";import{ae as d,ah as p,al as u,aj as o,ai as h,ak as m,ag as f}from"./index-3ASpM-P5.js";import{F as g,D as x,a}from"./DInputMask.stories-DYJ5e2kk.js";import{P as n}from"./config-CUZHf55T.js";import{r as b}from"./index-DH5ua8nC.js";import{D as j}from"./DAlert-4WmNgx47.js";import"./iframe-D_0qdknu.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./DInput-CpmK98WL.js";import"./index-mLFX9G4A.js";import"./DIcon-CDY5yPx9.js";import"./DContext-9gKMeQXa.js";import"./useProvidedRefOrCreate-DxC68qIt.js";import"./constants-DhWTmuJQ.js";const i="_ ____ ____";function k(){const[s,e]=b.useState(i);return t.jsx(g,{mask:i,showMask:!0,value:s,onChange:c=>e(c),replacement:{_:/\d/}})}function r(s){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",h4:"h4",img:"img",p:"p",strong:"strong",...l(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(d,{of:x}),`
+`,t.jsx(e.h1,{id:"input-mask",children:"Input Mask"}),`
+`,t.jsx(e.p,{children:t.jsx(e.img,{src:"https://img.shields.io/badge/Wrapper%20Component-red",alt:"Shield Badge"})}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.strong,{children:t.jsx(e.code,{children:"DInputMask"})}),` component is designed for creating masked input fields, leveraging
+the capabilities provided by `,t.jsx(e.strong,{children:"@react-input/mask"}),`.
+This component is composed with `,t.jsx(e.code,{children:"DInput"})," to offer a masked input functionality."]}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsxs(e.p,{children:[`To utilize this component effectively, it's important to understand how it interacts
+with state management, particularly when using `,t.jsx(e.code,{children:"useState"}),"."]}),`
+`,t.jsx(e.h3,{id:"initial-state",children:"Initial State"}),`
+`,t.jsxs(e.p,{children:["When using ",t.jsx(e.code,{children:"useState"}),` to manage the state of the DInputMask component, it's crucial to
+set the initial state to match the mask pattern. This ensures that the component
+behaves as expected throughout the input process.`]}),`
+`,t.jsxs(e.h4,{id:"important-note-from-react-inputmask-documentation",children:["Important Note from ",t.jsx(e.code,{children:"@react-input/mask"})," documentation"]}),`
+`,t.jsx(p,{children:t.jsx(j,{theme:"warning",children:t.jsx("span",{children:`The InputMask component does not change the value passed in the value or defaultValue property
+of the input element, so set the initialized value to something that can match the masked value
+at any stage of input. If you make a mistake, you will see a warning about it in the console.`})})}),`
+`,t.jsx(e.p,{children:`This means that setting the initial state correctly, in alignment with the mask
+pattern, is crucial for seamless functionality. Any discrepancies may result in warnings being displayed in the console.`}),`
+`,t.jsxs(e.p,{children:["For further details on the capabilities and usage of @react-input/mask, refer to the ",t.jsx(e.a,{href:"https://www.npmjs.com/package/@react-input/mask",rel:"nofollow",children:"documentation"}),"."]}),`
+`,t.jsx(u,{code:`
+import { useState } from 'react';
+import { DInputMask } from '@dynamic-framework/ui-react';
+
+const BASE_MASK = '_ ____ ____';
+
+export default function App() {
+  const [maskValue, setMaskValue] = useState<string>(BASE_MASK);
+
+  return (
+    <DInputMask
+      mask={BASE_MASK}
+      showMask
+      value={maskValue}
+      onChange={(value) => setMaskValue(value)}
+      replacement={{ _: /d/ }}
+    />
+  );
+}
+    `,language:"tsx",dark:!0}),`
+`,t.jsx(o,{children:t.jsx("div",{className:"d-flex justify-content-center",children:t.jsx(k,{})})}),`
+`,t.jsx(e.h2,{id:"css-variables",children:"CSS Variables"}),`
+`,t.jsxs(e.p,{children:["The Bootstrap documentation provides details on the default ",t.jsx(e.a,{href:"https://getbootstrap.com/docs/5.3/forms/form-control/#css",rel:"nofollow",children:"Input Form CSS Variables"}),`
+and so it does `,t.jsx(e.a,{href:"https://getbootstrap.com/docs/5.3/forms/input-group/#css",rel:"nofollow",children:"Input Group CSS Variables"})]}),`
+`,t.jsx(h,{children:`
+| Variable                                         | Type            | Description                   |
+|--------------------------------------------------|-----------------|-------------------------------|
+| --${n}label-color                        | css color unit  | Label color                   |
+| --${n}label-font-weight                  | css font weight | Label font weight             |
+| --${n}label-font-size                    | css length unit | Label font size               |
+| --${n}label-padding-x                    | css length unit | Label horizontal padding      |
+| --${n}label-padding-y                    | css length unit | Label vertical padding        |
+| --${n}input-border-color                 | css color unit  | Input border color            |
+| --${n}input-border-width                 | css length unit | Input border width            |
+| --${n}input-border-radius                | css length unit | Input border radius           |
+| --${n}input-focus-border-color           | css color unit  | Input focus border color      |
+| --${n}input-focus-box-shadow             | css shadow      | Input focus box shadow        |
+| --${n}input-disabled-bg                  | css color unit  | Input disable background      |
+| --${n}input-disabled-color               | css color unit  | Input disable color           |
+| --${n}input-disabled-border-color        | css color unit  | Input disable border color    |
+| --${n}form-text-padding                  | css length unit | Hint padding                  |
+| --${n}form-text-gap                      | css length unit | Space between hint elements   |
+| --${n}form-text-color                    | css color unit  | Hint color                    |
+| --${n}form-control-text-align            | css text align  | Input text align              |
+  `}),`
+`,t.jsx(o,{of:a}),`
+`,t.jsx(m,{of:a}),`
+`,t.jsx(f,{})]})}function N(s={}){const{wrapper:e}={...l(),...s.components};return e?t.jsx(e,{...s,children:t.jsx(r,{...s})}):r(s)}export{N as default};
