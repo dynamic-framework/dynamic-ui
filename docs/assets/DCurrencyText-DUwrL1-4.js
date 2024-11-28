3c3337d666f186ef14a196bdd2a9701d9662263d
abe-modyo <102051289+abe-modyo@users.noreply.github.com>
2024-11-28 23:43:39 +0000

    doc: update storybook

---
+++ docs/assets/DCurrencyText-DUwrL1-4.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as i}from"./index-COxJNofV.js";import{ae as s,af as m,ag as c}from"./index-Cp_CK22P.js";import{D as e}from"./DCurrencyText.stories-BRMw7EHY.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-34Tcr5dh.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./useFormatCurrency-49TXw5x4.js";import"./currency.es-9OAR_aOO.js";import"./DContext-9gKMeQXa.js";function n(o){const r={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r.h1,{id:"currency-text",children:"Currency Text"}),`
+`,t.jsxs(r.p,{children:["Component to format and display a currency value using a ",t.jsx(r.code,{children:"DContextProvider"})," to get the config."]}),`
+`,t.jsx(r.h2,{id:"args",children:"Args"}),`
+`,t.jsx(s,{of:e}),`
+`,t.jsx(m,{of:e,sort:"requiredFirst"}),`
+`,t.jsx(c,{})]})}function v(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{v as default};
