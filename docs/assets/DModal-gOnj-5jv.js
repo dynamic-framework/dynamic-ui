d6fdcb95404ed5f477e75563db65d8e550dea215
abe-modyo <102051289+abe-modyo@users.noreply.github.com>
2024-11-28 21:47:30 +0000

    doc: update storybook

---
+++ docs/assets/DModal-gOnj-5jv.js
@@ -0,0 +1,18 @@
+import{j as o}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as a}from"./index-COxJNofV.js";import{ae as i,ah as d,ai as l,aj as c,ak as m,ag as h}from"./index-BZTHR144.js";import{D as p,a as s}from"./DModal.stories-79QooIiX.js";import{P as t}from"./config-CUZHf55T.js";import{D as x}from"./DAlert-4WmNgx47.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Ddac0JGS.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./DModal-Cm_MCYvt.js";import"./index-mLFX9G4A.js";import"./DIcon-CDY5yPx9.js";import"./DContext-9gKMeQXa.js";import"./DButton-Cbcj12Fm.js";import"./constants-DhWTmuJQ.js";function n(r){const e={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",strong:"strong",...a(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
+`,o.jsx(e.h1,{id:"modal",children:"Modal"}),`
+`,o.jsxs(e.p,{children:["This section provides guidance specifically for using ",o.jsx(e.code,{children:"DModal"})," in ",o.jsx(e.strong,{children:"inline"})," scenarios."]}),`
+`,o.jsx(d,{children:o.jsx(x,{theme:"warning",children:o.jsxs("span",{children:["To achieve the behavior of a modal it is necessary to use the ",o.jsx(e.strong,{children:o.jsx(e.code,{children:"DContextProvider"})})," and the ",o.jsx(e.strong,{children:o.jsx(e.code,{children:"useDPortalContext"})}),` hook.
+For detailed guidance on the `,o.jsx(e.strong,{children:"correct usage"})," of modals, please refer to the ",o.jsx(e.a,{href:"/docs/design-system-patterns-modal--docs",children:"Modal pattern section"})," in our documentation."]})})}),`
+`,o.jsx(e.h2,{id:"css-variables",children:"CSS Variables"}),`
+`,o.jsx(l,{children:`
+| Variable                               | Class  | Type            | Description                  |
+|----------------------------------------|--------|-----------------|------------------------------|
+| --${t}modal-header-gap         | .modal | css length unit | Space between header items   |
+| --${t}modal-footer-padding     | .modal | css length unit | Footer padding               |
+| --${t}modal-separator-margin-x | .modal | css length unit | Separator horizontal padding |
+| --${t}modal-separator-height   | .modal | css length unit | Separator height (size)      |
+| --${t}modal-separator-bg       | .modal | css color unit  | Separator background (color) |
+  `}),`
+`,o.jsx(c,{of:s}),`
+`,o.jsx(m,{of:s}),`
+`,o.jsx(h,{})]})}function R(r={}){const{wrapper:e}={...a(),...r.components};return e?o.jsx(e,{...r,children:o.jsx(n,{...r})}):n(r)}export{R as default};
---
+++ docs/assets/DPopover-C98xRmQ2.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as i}from"./index-COxJNofV.js";import{ae as s,ah as a,aj as m,ak as p,ag as l}from"./index-BZTHR144.js";import{D as c,a as n}from"./DPopover.stories-el5AQMRk.js";import{D as d}from"./DAlert-4WmNgx47.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Ddac0JGS.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./index-mLFX9G4A.js";import"./floating-ui.react-CzTYqOs8.js";import"./floating-ui.dom-B5CW5bK0.js";import"./config-CUZHf55T.js";import"./DQuickActionButton-B1EnqC80.js";import"./DIcon-CDY5yPx9.js";import"./DContext-9gKMeQXa.js";import"./DButton-Cbcj12Fm.js";function r(t){const e={a:"a",h1:"h1",img:"img",li:"li",p:"p",ul:"ul",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:c}),`
+`,o.jsx(e.h1,{id:"popover",children:"Popover"}),`
+`,o.jsx(e.p,{children:o.jsx(e.img,{src:"https://img.shields.io/badge/Abstraction%20Component-4848b7",alt:"Shield Badge"})}),`
+`,o.jsx(a,{children:o.jsx(d,{theme:"danger",children:o.jsx("span",{children:"This component is deprecated and will be removed in the next major release, we are working on a replacement."})})}),`
+`,o.jsx(e.p,{children:"This component only complies with the behavior of a popover, it has no design, other components are used as an example to assemble it."}),`
+`,o.jsx(e.p,{children:"To understand in more detail the aspects covered by this component, review the following documentation:"}),`
+`,o.jsxs(e.ul,{children:[`
+`,o.jsx(e.li,{children:o.jsx(e.a,{href:"https://floating-ui.com/docs/react",rel:"nofollow",children:"Floating UI"})}),`
+`]}),`
+`,o.jsx(m,{of:n}),`
+`,o.jsx(p,{of:n}),`
+`,o.jsx(l,{})]})}function B(t={}){const{wrapper:e}={...i(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(r,{...t})}):r(t)}export{B as default};
