627422fcf5301c4f8872e8c63b0d4fcf406c31b6
abe-modyo <102051289+abe-modyo@users.noreply.github.com>
2024-11-28 23:41:29 +0000

    doc: update storybook

---
+++ docs/assets/DList-DSbDcqH3.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as r}from"./index-COxJNofV.js";import{ae as i,ah as a,aj as l,ak as p,ag as m}from"./index-vWmdiQem.js";import{D as d,a as s}from"./DList.stories-DGmjpbC-.js";import{D as c}from"./DAlert-4WmNgx47.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Cpb8CKhn.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./DList-CuPEhQSU.js";import"./index-mLFX9G4A.js";import"./DIcon-CDY5yPx9.js";import"./config-CUZHf55T.js";import"./DContext-9gKMeQXa.js";function n(e){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:d}),`
+`,t.jsx(o.h1,{id:"list",children:"List"}),`
+`,t.jsx(a,{children:t.jsx(c,{theme:"danger",children:t.jsxs("span",{children:[`This component is deprecated and will be removed in a future release.
+Please avoid using it and instead use the `,t.jsx(o.a,{href:"/docs/design-system-components-listgroup--docs",children:"DListGroup"})]})})}),`
+`,t.jsx(o.p,{children:"List of elements"}),`
+`,t.jsx(o.p,{children:"To understand in more detail the aspects covered by this component, review the following documentation:"}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsx(o.li,{children:t.jsx(o.a,{href:"https://getbootstrap.com/docs/5.3/components/list-group/",rel:"nofollow",children:"Bootstrap List Group"})}),`
+`]}),`
+`,t.jsx(o.h2,{id:"css-variables",children:"CSS Variables"}),`
+`,t.jsxs(o.p,{children:["The Bootstrap documentation provides details on the default ",t.jsx(o.a,{href:"https://getbootstrap.com/docs/5.3/components/list-group/#css",rel:"nofollow",children:"List Group CSS Variables"})]}),`
+`,t.jsx(l,{of:s}),`
+`,t.jsx(p,{of:s}),`
+`,t.jsx(m,{})]})}function V(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n(e)}export{V as default};
---
+++ docs/assets/useFormatCurrency-CZvkXUA4.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-DQ32znRX.js";import{useMDXComponents as s}from"./index-COxJNofV.js";import{ae as i,al as o,ah as a}from"./index-vWmdiQem.js";import{u as m}from"./useFormatCurrency-49TXw5x4.js";import{D as p}from"./DContext-9gKMeQXa.js";import"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Cpb8CKhn.js";import"../sb-preview/runtime.js";import"./index-D9uilScg.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-DULPOR7A.js";import"./index-C9uyZMOe.js";import"./index-DrFu-skq.js";import"./currency.es-9OAR_aOO.js";function c(){const{values:t}=m(100,234.12,-233);return e.jsx("div",{className:"p-3",children:t.map(r=>e.jsx("pre",{children:r},r))})}function x(){return e.jsx(p,{children:e.jsx(c,{})})}function n(t){const r={code:"code",h1:"h1",h2:"h2",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Design System/Utils/Hooks/useFormatCurrency"}),`
+`,e.jsx(r.h1,{id:"useformatcurrency",children:"useFormatCurrency"}),`
+`,e.jsx(r.p,{children:"Currency formatting hook"}),`
+`,e.jsxs(r.h2,{id:"setup-apptsx",children:["Setup ",e.jsx(r.code,{children:"App.tsx"})]}),`
+`,e.jsx(r.p,{children:"It is necessary to use the hook inside a DContextProvider, because the hook takes the formatting configuration from that context"}),`
+`,e.jsx(o,{code:`
+import {
+  DContextProvider,
+} from '@dynamic-framework/ui-react';
+
+import ExampleOfUse from './ExampleOfUse';
+
+export function App() {
+  return (
+    <DContextProvider>
+      <ExampleOfUse />
+    </DContextProvider>
+  );
+}
+    `,language:"tsx",dark:!0}),`
+`,e.jsxs(r.h2,{id:"example-of-use-exampleusagetsx",children:["Example of use ",e.jsx(r.code,{children:"ExampleUsage.tsx"})]}),`
+`,e.jsx(r.p,{children:"Component that uses the hook"}),`
+`,e.jsx(o,{code:`
+import {
+  useFormatCurrency,
+} from '@dynamic-framework/ui-react';
+
+export function ExampleOfUse() {
+  const { values } = useFormatCurrency(100, 234.12, -233);
+  return (
+    <div className="p-3">
+      {values.map((value) => (
+        <pre key={value}>{value}</pre>
+      ))}
+    </div>
+  );
+}
+    `,language:"tsx",dark:!0}),`
+`,e.jsxs(r.h2,{id:"setup-with-other-settings-apptsx",children:["Setup with other settings ",e.jsx(r.code,{children:"App.tsx"})]}),`
+`,e.jsx(r.p,{children:"Formatting settings can be changed by changing the values of the currency property of the DContextProvider"}),`
+`,e.jsx(o,{code:`
+import {
+  DContextProvider,
+} from '@dynamic-framework/ui-react';
+
+import ExampleOfUse from './ExampleOfUse';
+
+export function App() {
+  return (
+    <DContextProvider
+      currency={{
+        symbol: '$',
+        precision: 2,
+        separator: ',',
+        decimal: '.',
+      }}
+    >
+      <ExampleOfUse />
+    </DContextProvider>
+  );
+}
+    `,language:"tsx",dark:!0}),`
+`,e.jsx(r.h2,{id:"example-render",children:"Example Render"}),`
+`,e.jsx(a,{children:e.jsx(x,{})})]})}function O(t={}){const{wrapper:r}={...s(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(n,{...t})}):n(t)}export{O as default};
