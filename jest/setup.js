c136139fb845a602581e34574f41378bd7095617
Johann F Rojas <43022731+johannrp27@users.noreply.github.com>
2025-02-21 17:09:41 -0500

    fix: add matchMedia property as global in jest (#801)

--- jest/setup.js
+++ jest/setup.js
@@ -28,18 +28,3 @@ Object.defineProperty(global.window, 'matchMedia', {
     dispatchEvent: jest.fn(),
   })),
 });
-
-// eslint-disable-next-line no-undef
-globalThis.ResizeObserver = class {
-  observe() {
-    return this;
-  }
-
-  unobserve() {
-    return this;
-  }
-
-  disconnect() {
-    return this;
-  }
-};
