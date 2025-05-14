43a1d6c8e064b8cee8be7aa139645e6f7ec14942
johannrp27 <johannrp27@users.noreply.github.com>
2025-05-14 02:56:28 +0000

    doc: update storybook

---
+++ docs/assets/DInputSearch.stories-TWTQ0BYf.js
@@ -0,0 +1,96 @@
+import{j as d}from"./jsx-runtime-Yb_4y5hn.js";import{r as T}from"./index-BwDkhjyp.js";import{F as $}from"./DInput-Cx_Zrwj0.js";import{u as w}from"./useProvidedRefOrCreate-DoZI9Y9Y.js";import{I as M,a as N}from"./constants-BfTaJ1B2.js";import{P as e}from"./config-BV4l-vIF.js";import{D as R}from"./DContext-K5yUkfPZ.js";function s({type:i,iconEnd:j,iconEndAriaLabel:D="search",...F},A){const _=w(A);return d.jsx($,{ref:_,type:"text",iconEnd:"search",iconEndAriaLabel:D,...F})}const u=T.forwardRef(s);u.displayName="DInputSearch";try{s.displayName="DInputSearch",s.__docgenInfo={description:"",displayName:"DInputSearch",props:{style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties | undefined"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string | undefined"}},dataAttributes:{defaultValue:null,description:"",name:"dataAttributes",required:!1,type:{name:"DataAttributes | undefined"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"sm"'},{value:'"lg"'}]}},iconFamilyClass:{defaultValue:null,description:"",name:"iconFamilyClass",required:!1,type:{name:"string | undefined"}},iconFamilyPrefix:{defaultValue:null,description:"",name:"iconFamilyPrefix",required:!1,type:{name:"string | undefined"}},iconMaterialStyle:{defaultValue:null,description:"",name:"iconMaterialStyle",required:!1,type:{name:"boolean | undefined"}},iconStart:{defaultValue:null,description:"",name:"iconStart",required:!1,type:{name:"string | undefined"}},iconEnd:{defaultValue:null,description:"",name:"iconEnd",required:!1,type:{name:"string | undefined"}},value:{defaultValue:null,description:"",name:"value",required:!1,type:{name:"string | undefined"}},iconStartDisabled:{defaultValue:null,description:"",name:"iconStartDisabled",required:!1,type:{name:"boolean | undefined"}},iconStartFamilyClass:{defaultValue:null,description:"",name:"iconStartFamilyClass",required:!1,type:{name:"string | undefined"}},iconStartFamilyPrefix:{defaultValue:null,description:"",name:"iconStartFamilyPrefix",required:!1,type:{name:"string | undefined"}},iconStartAriaLabel:{defaultValue:null,description:"",name:"iconStartAriaLabel",required:!1,type:{name:"string | undefined"}},iconStartTabIndex:{defaultValue:null,description:"",name:"iconStartTabIndex",required:!1,type:{name:"number | undefined"}},iconStartMaterialStyle:{defaultValue:null,description:"",name:"iconStartMaterialStyle",required:!1,type:{name:"boolean | undefined"}},iconEndDisabled:{defaultValue:null,description:"",name:"iconEndDisabled",required:!1,type:{name:"boolean | undefined"}},iconEndFamilyClass:{defaultValue:null,description:"",name:"iconEndFamilyClass",required:!1,type:{name:"string | undefined"}},iconEndFamilyPrefix:{defaultValue:null,description:"",name:"iconEndFamilyPrefix",required:!1,type:{name:"string | undefined"}},iconEndAriaLabel:{defaultValue:null,description:"",name:"iconEndAriaLabel",required:!1,type:{name:"string | undefined"}},iconEndTabIndex:{defaultValue:null,description:"",name:"iconEndTabIndex",required:!1,type:{name:"number | undefined"}},iconEndMaterialStyle:{defaultValue:null,description:"",name:"iconEndMaterialStyle",required:!1,type:{name:"boolean | undefined"}},loading:{defaultValue:null,description:"",name:"loading",required:!1,type:{name:"boolean | undefined"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"((value: string) => void) | undefined"}},label:{defaultValue:null,description:"",name:"label",required:!1,type:{name:"string | undefined"}},invalid:{defaultValue:null,description:"",name:"invalid",required:!1,type:{name:"boolean | undefined"}},valid:{defaultValue:null,description:"",name:"valid",required:!1,type:{name:"boolean | undefined"}},labelIcon:{defaultValue:null,description:"",name:"labelIcon",required:!1,type:{name:"string | undefined"}},labelIconFamilyClass:{defaultValue:null,description:"",name:"labelIconFamilyClass",required:!1,type:{name:"string | undefined"}},labelIconFamilyPrefix:{defaultValue:null,description:"",name:"labelIconFamilyPrefix",required:!1,type:{name:"string | undefined"}},labelIconMaterialStyle:{defaultValue:null,description:"",name:"labelIconMaterialStyle",required:!1,type:{name:"boolean | undefined"}},hint:{defaultValue:null,description:"",name:"hint",required:!1,type:{name:"string | undefined"}},floatingLabel:{defaultValue:null,description:"",name:"floatingLabel",required:!1,type:{name:"boolean | undefined"}},inputStart:{defaultValue:null,description:"",name:"inputStart",required:!1,type:{name:"ReactNode"}},inputEnd:{defaultValue:null,description:"",name:"inputEnd",required:!1,type:{name:"ReactNode"}},onIconStartClick:{defaultValue:null,description:"",name:"onIconStartClick",required:!1,type:{name:"((value?: string | undefined) => void) | undefined"}},onIconEndClick:{defaultValue:null,description:"",name:"onIconEndClick",required:!1,type:{name:"((value?: string | undefined) => void) | undefined"}}}}}catch{}const O={title:"Design System/Components/Input Search",component:u,parameters:{docs:{description:{component:`
+Component composition with \`d-input\` to make a search input component.
+
+## CSS Variables
+
+The Bootstrap documentation provides details on the default [Input Form CSS Variables](https://getbootstrap.com/docs/5.3/forms/form-control/#css)
+and so it does [Input Group CSS Variables](https://getbootstrap.com/docs/5.3/forms/input-group/#css)
+
+| Variable                                  | Class         | Type            | Description                 |
+|-------------------------------------------|---------------|-----------------|-----------------------------|
+| --${e}label-color                 | :root         | css color unit  | Label color                 |
+| --${e}label-font-weight           | :root         | css font weight | Label font weight           |
+| --${e}label-font-size             | :root         | css length unit | Label font size             |
+| --${e}label-padding-x             | :root         | css length unit | Label horizontal padding    |
+| --${e}label-padding-y             | :root         | css length unit | Label vertical padding      |
+| --${e}input-border-color          | .input-group  | css color unit  | Input border color          |
+| --${e}input-border-width          | .input-group  | css length unit | Input border width          |
+| --${e}input-border-radius         | .input-group  | css length unit | Input border radius         |
+| --${e}input-focus-border-color    | .input-group  | css color unit  | Input focus border color    |
+| --${e}input-focus-box-shadow      | .input-group  | css shadow      | Input focus box shadow      |
+| --${e}input-disabled-bg           | .input-group  | css color unit  | Input disable background    |
+| --${e}input-disabled-color        | .input-group  | css color unit  | Input disable color         |
+| --${e}input-disabled-border-color | .input-group  | css color unit  | Input disable border color  |
+| --${e}form-text-padding           | .form-text    | css length unit | Hint padding                |
+| --${e}form-text-gap               | .form-text    | css length unit | Space between hint elements |
+| --${e}form-text-color             | .form-text    | css color unit  | Hint color                  |
+| --${e}form-control-text-align     | .form-control | css text align  | Input text align            |
+        `}}},argTypes:{id:{control:"text",type:"string",description:"The id of the input"},name:{control:"text",type:"string",description:"The name of the input"},className:{control:"text",type:"string"},style:{control:"object"},label:{control:"text",type:"string"},labelIcon:{control:{type:"select",labels:{undefined:"empty"}},type:"string",options:[void 0,...M]},placeholder:{control:"text",type:"string"},value:{control:"text",type:"string",description:"The value of the input"},size:{control:{type:"select",labels:{undefined:"empty"}},type:"string",options:[void 0,"sm","lg"]},disabled:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},readOnly:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},loading:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},hint:{control:"text",type:"string",description:"Hint to display, also used to display validity feedback"},iconEndAriaLabel:{control:"text",type:"string"},invalid:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},valid:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},floatingLabel:{control:"boolean",type:"boolean",table:{defaultValue:{summary:"false"}}},onClick:{action:"onClick"},onChange:{action:"onChange"}}},n={args:{id:"componentId1",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,iconEndAriaLabel:"search"}},a={args:{id:"componentId3",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,invalid:!0,iconEndAriaLabel:"search"}},l={args:{id:"componentId4",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,valid:!0,iconEndAriaLabel:"search"}},t={args:{id:"componentId5",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,disabled:!0,iconEndAriaLabel:"search"}},o={args:{id:"componentId6",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,iconEndAriaLabel:"search",floatingLabel:!0}},r={render:i=>d.jsx(R,{...N,children:d.jsx(u,{...i})}),args:{id:"componentId7",label:"Label",placeholder:"Placeholder",labelIcon:void 0,value:void 0,iconEndAriaLabel:"search"},parameters:{docs:{canvas:{sourceState:"shown"}}}};var c,p,m;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    id: 'componentId1',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    iconEndAriaLabel: 'search'
+  }
+}`,...(m=(p=n.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var f,b,y;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    id: 'componentId3',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    invalid: true,
+    iconEndAriaLabel: 'search'
+  }
+}`,...(y=(b=a.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var g,h,I;l.parameters={...l.parameters,docs:{...(g=l.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    id: 'componentId4',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    valid: true,
+    iconEndAriaLabel: 'search'
+  }
+}`,...(I=(h=l.parameters)==null?void 0:h.docs)==null?void 0:I.source}}};var v,S,V;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    id: 'componentId5',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    disabled: true,
+    iconEndAriaLabel: 'search'
+  }
+}`,...(V=(S=t.parameters)==null?void 0:S.docs)==null?void 0:V.source}}};var x,E,L;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    id: 'componentId6',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    iconEndAriaLabel: 'search',
+    floatingLabel: true
+  }
+}`,...(L=(E=o.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};var q,C,P;r.parameters={...r.parameters,docs:{...(q=r.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: (args: ComponentProps<typeof DInputSearch>) => <DContextProvider {...CONTEXT_PROVIDER_CONFIG_MATERIAL}>
+      <DInputSearch {...args} />
+    </DContextProvider>,
+  args: {
+    id: 'componentId7',
+    label: 'Label',
+    placeholder: 'Placeholder',
+    labelIcon: undefined,
+    value: undefined,
+    iconEndAriaLabel: 'search'
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown'
+      }
+    }
+  }
+}`,...(P=(C=r.parameters)==null?void 0:C.docs)==null?void 0:P.source}}};const k=["Default","Invalid","Valid","Disabled","Floating","MaterialIcon"],Q=Object.freeze(Object.defineProperty({__proto__:null,Default:n,Disabled:t,Floating:o,Invalid:a,MaterialIcon:r,Valid:l,__namedExportsOrder:k,default:O},Symbol.toStringTag,{value:"Module"}));export{Q as D,n as a};
