bdcdc0a2dba4512de8a2c8722544779efc0fd761
Eduard Baron <42986303+efbarong@users.noreply.github.com>
2025-05-26 11:37:03 -0500

    chore: update test configuration and add render test for DPaginator (#838)

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// eslint-disable-next-line no-undef
+globalThis.ResizeObserver = class {
+  observe() {
+    return this;
+  }
+
+  unobserve() {
+    return this;
+  }
+
+  disconnect() {
+    return this;
+  }
+};
