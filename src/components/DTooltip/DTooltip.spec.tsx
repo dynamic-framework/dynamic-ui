2867d16db5284e4f1696c28da98be49c7a365a93
lnazarit <lnazarit@modyo.com>
2025-05-30 14:56:07 -0400

    test(DTooltip): tests for DTooltip, added user-event library

--- src/components/DTooltip/DTooltip.spec.tsx
+++ src/components/DTooltip/DTooltip.spec.tsx
@@ -1,21 +1,128 @@
-import { render } from '@testing-library/react';
-import { ComponentProps } from 'react';
-import DTooltip from '.';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  within,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import DTooltip from './DTooltip'; // Ajusta la ruta si es necesario
 
-it('should render a tooltip', () => {
-  const props: ComponentProps<typeof DTooltip> = {
-    Component: <>Text to hover</>,
-  };
+describe('DTooltip', () => {
+  const tooltipText = 'Tooltip text';
 
-  const { container } = render(
-    <DTooltip {...props} />,
-  );
+  it('Render component', () => {
+    render(
+      <DTooltip Component={<span>Hover me</span>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+    expect(screen.getByText('Hover me')).toBeInTheDocument();
+  });
 
-  expect(container).toMatchInlineSnapshot(`
-    <div>
-      <div>
-        Text to hover
-      </div>
-    </div>
-  `);
+  it('Default tooltip should show on hover', async () => {
+    render(
+      <DTooltip withHover Component={<span>Hover me</span>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    const button = screen.getByText('Hover me');
+
+    await act(async () => {
+      await userEvent.hover(button);
+    });
+
+    await waitFor(() => {
+      expect(within(document.body).getByText(tooltipText)).toBeInTheDocument();
+    });
+  });
+
+  it('Show tooltip on Click event', async () => {
+    render(
+      <DTooltip withClick Component={<button type="button">Click me</button>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    const button = screen.getByText('Click me');
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText(tooltipText)).toBeInTheDocument();
+    });
+  });
+
+  it('Show tooltip inmediatly if open=true', () => {
+    render(
+      <DTooltip open Component={<span>Tooltip target</span>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    expect(screen.getByText(tooltipText)).toBeInTheDocument();
+  });
+
+  it('Not show tooltip if open=false and not interacted', () => {
+    render(
+      <DTooltip open={false} Component={<span>No tooltip yet</span>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    expect(screen.queryByText(tooltipText)).not.toBeInTheDocument();
+  });
+
+  it('Custom classnames should be applied, size and theme', () => {
+    render(
+      <DTooltip
+        open
+        className="custom-tooltip"
+        theme="light"
+        size="sm"
+        Component={<span>Target</span>}
+      >
+        {tooltipText}
+      </DTooltip>,
+    );
+    const tooltip = screen.getByText(tooltipText).closest('div.tooltip');
+    expect(tooltip).toHaveClass('tooltip-light');
+    expect(tooltip).toHaveClass('tooltip-sm');
+    expect(tooltip).toHaveClass('custom-tooltip');
+  });
+
+  it('Text danger theme should show tooltip with tooltip-danger classname', () => {
+    render(
+      <DTooltip
+        open
+        theme="danger"
+        Component={<span>Target</span>}
+      >
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    const tooltip = screen.getByText(tooltipText).closest('div.tooltip');
+    expect(tooltip).toHaveClass('tooltip-danger');
+  });
+
+  it('Text withFocus should show tooltip', async () => {
+    render(
+      <DTooltip withFocus Component={<button type="button">Focus me</button>}>
+        {tooltipText}
+      </DTooltip>,
+    );
+
+    const button = screen.getByText('Focus me');
+    await act(async () => {
+      await userEvent.tab();
+    });
+    fireEvent.focus(button);
+
+    await waitFor(() => {
+      expect(screen.getByText(tooltipText)).toBeInTheDocument();
+    });
+  });
 });
