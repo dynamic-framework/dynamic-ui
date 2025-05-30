55067e7e1e1cc2ae66839dc7bf3d26aaea29af71
Eduard Baron <ebaron@modyo.com>
2025-05-30 17:20:58 -0500

    test(DInputPassword): improve coverage

--- src/components/DInputPassword/DInputPassword.spec.tsx
+++ src/components/DInputPassword/DInputPassword.spec.tsx
@@ -1,24 +1,25 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import DInputPassword from './DInputPassword';
 
-it('should render my component', () => {
-  const search = {
-    id: 'passwordId',
-    label: 'Label',
-    value: 'Value',
-    placeholder: 'Password',
-  };
+describe('<DInputPassword />', () => {
+  it('should render my component', () => {
+    const search = {
+      id: 'passwordId',
+      label: 'Label',
+      value: 'Value',
+      placeholder: 'Password',
+    };
 
-  const { container } = render(
-    <DInputPassword
-      id={search.id}
-      label={search.label}
-      value={search.value}
-      placeholder={search.placeholder}
-    />,
-  );
+    const { container } = render(
+      <DInputPassword
+        id={search.id}
+        label={search.label}
+        value={search.value}
+        placeholder={search.placeholder}
+      />,
+    );
 
-  expect(container).toMatchInlineSnapshot(`
+    expect(container).toMatchInlineSnapshot(`
     <div>
       <div>
         <label
@@ -52,4 +53,41 @@ it('should render my component', () => {
       </div>
     </div>
   `);
+  });
+
+  it('renders input type=password initially', () => {
+    render(
+      <DInputPassword aria-label="input password" />,
+    );
+
+    const input = screen.getByLabelText('input password');
+    expect(input).toHaveAttribute('type', 'password');
+  });
+
+  it('toggles visibility when icon is clicked', () => {
+    render(
+      <DInputPassword aria-label="input password" />,
+    );
+
+    const iconButton = screen.getByLabelText('show/hide password');
+    const input = screen.getByLabelText('input password');
+
+    expect(input).toHaveAttribute('type', 'password');
+    fireEvent.click(iconButton);
+    expect(input).toHaveAttribute('type', 'text');
+    fireEvent.click(iconButton);
+    expect(input).toHaveAttribute('type', 'password');
+  });
+
+  it('calls onIconEndClick if provided', () => {
+    const handleClick = jest.fn();
+
+    render(
+      <DInputPassword onIconEndClick={handleClick} />,
+    );
+
+    const iconButton = screen.getByLabelText('show/hide password');
+    fireEvent.click(iconButton);
+    expect(handleClick).toHaveBeenCalled();
+  });
 });
