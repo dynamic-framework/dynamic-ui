7fc65443abe756de4e87d133220f40074ef20010
Eduard Baron <ebaron@modyo.com>
2025-07-18 18:18:07 -0500

    chore: update dependencies and setup for storybook 8.6.14

---
+++ vite.config.mjs
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./.storybook/test-setup.ts'],
+  },
+});
