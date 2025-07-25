525660b947e56e016be622c044374acf8082a3ab
Roberto Rivera S. <rrivera@modyo.com>
2025-07-25 17:13:51 -0400

    fix: copilot feedback

--- src/components/DCarousel/components/DCarouselSlide.tsx
+++ src/components/DCarousel/components/DCarouselSlide.tsx
@@ -11,7 +11,7 @@ export default function DCarouselSlide({ className, ...props }: Props) {
       {...props}
       className={classNames(
         'd-carousel-slide',
-        className as string,
+        className,
       )}
     />
   );
