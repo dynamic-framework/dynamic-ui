77a013daf676575fe40b1e3386beef8a43686334
Eduard Baron <ebaron@modyo.com>
2025-08-05 13:01:42 -0500

    chore: add testable mock for DPaginator

--- jest/setup.js
+++ jest/setup.js
@@ -1,6 +1,56 @@
+/* eslint-disable react/prop-types */
+const React = require('react');
+
 jest.mock('react-content-loader', () => jest.fn());
 
-jest.mock('react-responsive-pagination', () => jest.fn());
+jest.mock('react-responsive-pagination', () => {
+  function MockPagination(props) {
+    const {
+      current,
+      total,
+      onPageChange,
+      className = '',
+    } = props;
+
+    const handleClick = (page) => {
+      if (page !== current && page >= 1 && page <= total) {
+        onPageChange(page);
+      }
+    };
+
+    return React.createElement('div', {
+      className,
+      'data-testid': 'responsive-pagination',
+    }, [
+      React.createElement('button', {
+        type: 'button',
+        key: 'prev',
+        onClick: () => handleClick(current - 1),
+        disabled: current === 1,
+        'data-testid': 'pagination-previous',
+      }, 'Previous'),
+
+      ...Array.from({ length: total }, (_, i) => i + 1).map((page) => React.createElement('button', {
+        key: page,
+        type: 'button',
+        onClick: () => handleClick(page),
+        'data-testid': `pagination-page-${page}`,
+        'aria-current': page === current ? 'page' : undefined,
+        style: { fontWeight: page === current ? 'bold' : 'normal' },
+      }, page)),
+
+      React.createElement('button', {
+        key: 'next',
+        type: 'button',
+        onClick: () => handleClick(current + 1),
+        disabled: current === total,
+        'data-testid': 'pagination-next',
+      }, 'Next'),
+    ]);
+  }
+
+  return MockPagination;
+});
 
 jest.mock('@react-input/mask', () => ({
   InputMask: jest.fn(),
