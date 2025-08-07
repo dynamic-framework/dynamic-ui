9f3035f01f50325d9bac93085a71088f1a222e1e
pipemeyer <fmeyer@modyo.com>
2025-08-07 10:39:36 -0400

    test: reorder DOffcanvas

--- src/components/DOffcanvas/DOffcanvas.spec.tsx
+++ src/components/DOffcanvas/DOffcanvas.spec.tsx
@@ -20,73 +20,75 @@ jest.mock('../../contexts', () => ({
 }));
 
 describe('<DOffcanvas />', () => {
-  it('should render my component with header, body, and footer', () => {
-    const { container } = render(
-      <DOffcanvas name="myOffcanvas">
-        <DOffcanvas.Header>Test Header</DOffcanvas.Header>
-        <DOffcanvas.Body>Test Body</DOffcanvas.Body>
-        <DOffcanvas.Footer>Test Footer</DOffcanvas.Footer>
-      </DOffcanvas>,
-    );
+  describe('Rendering and Props', () => {
+    it('should render with header, body, and footer', () => {
+      const { container } = render(
+        <DOffcanvas name="myOffcanvas">
+          <DOffcanvas.Header>Test Header</DOffcanvas.Header>
+          <DOffcanvas.Body>Test Body</DOffcanvas.Body>
+          <DOffcanvas.Footer>Test Footer</DOffcanvas.Footer>
+        </DOffcanvas>,
+      );
 
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <div
-          aria-hidden="false"
-          aria-labelledby="myOffcanvasLabel"
-          class="offcanvas portal show offcanvas-end"
-          id="myOffcanvas"
-          tabindex="-1"
-        >
+      expect(container).toMatchInlineSnapshot(`
+        <div>
           <div
-            class="offcanvas-header"
+            aria-hidden="false"
+            aria-labelledby="myOffcanvasLabel"
+            class="offcanvas portal show offcanvas-end"
+            id="myOffcanvas"
+            tabindex="-1"
           >
-            <div>
-              Test Header
+            <div
+              class="offcanvas-header"
+            >
+              <div>
+                Test Header
+              </div>
+            </div>
+            <div
+              class="d-offcanvas-separator"
+            />
+            <div
+              class="offcanvas-body"
+            >
+              Test Body
+            </div>
+            <div
+              class="d-offcanvas-separator"
+            />
+            <div
+              class="d-offcanvas-footer"
+            >
+              Test Footer
             </div>
           </div>
-          <div
-            class="d-offcanvas-separator"
-          />
-          <div
-            class="offcanvas-body"
-          >
-            Test Body
-          </div>
-          <div
-            class="d-offcanvas-separator"
-          />
-          <div
-            class="d-offcanvas-footer"
-          >
-            Test Footer
-          </div>
         </div>
-      </div>
-    `);
-  });
+      `);
+    });
 
-  it.each([
-    ['start', 'offcanvas-start'],
-    ['top', 'offcanvas-top'],
-    ['bottom', 'offcanvas-bottom'],
-  ])('renders on position %s', (position, expectedClass) => {
-    const { container } = render(<DOffcanvas name="test" openFrom={position as 'start' | 'top' | 'bottom'} />);
-    expect(container.firstChild).toHaveClass(expectedClass);
-  });
+    it.each([
+      ['start', 'offcanvas-start'],
+      ['top', 'offcanvas-top'],
+      ['bottom', 'offcanvas-bottom'],
+    ])('should render on position %s', (position, expectedClass) => {
+      const { container } = render(<DOffcanvas name="test" openFrom={position as 'start' | 'top' | 'bottom'} />);
+      expect(container.firstChild).toHaveClass(expectedClass);
+    });
 
-  it('renders with a static backdrop', () => {
-    const { container } = render(<DOffcanvas name="test" staticBackdrop />);
-    expect(container.firstChild).toHaveAttribute('data-bs-backdrop', 'static');
-  });
+    it('should render with a static backdrop', () => {
+      const { container } = render(<DOffcanvas name="test" staticBackdrop />);
+      expect(container.firstChild).toHaveAttribute('data-bs-backdrop', 'static');
+    });
 
-  it('renders a scrollable offcanvas', () => {
-    const { container } = render(<DOffcanvas name="test" scrollable />);
-    expect(container.firstChild).toHaveAttribute('data-bs-scroll', 'true');
+    it('should render a scrollable offcanvas', () => {
+      const { container } = render(<DOffcanvas name="test" scrollable />);
+      expect(container.firstChild).toHaveAttribute('data-bs-scroll', 'true');
+    });
   });
 
   describe('<DOffcanvas.Header />', () => {
-    it('renders a close button and calls onClose when clicked', async () => {
+    it('should render a close button and call onClose when clicked', async () => {
       const user = userEvent.setup();
       const handleClose = jest.fn();
       render(
@@ -104,14 +106,14 @@ describe('<DOffcanvas />', () => {
   });
 
   describe('<DOffcanvas.Body />', () => {
-    it('renders children correctly', () => {
+    it('should render children correctly', () => {
       render(<DOffcanvas.Body>Offcanvas Body Content</DOffcanvas.Body>);
       expect(screen.getByText('Offcanvas Body Content')).toBeInTheDocument();
     });
   });
 
   describe('<DOffcanvas.Footer />', () => {
-    it('applies action placement class', () => {
+    it('should apply an action placement class', () => {
       const { container } = render(
         <DOffcanvas.Footer actionPlacement="start">
           Footer Content
