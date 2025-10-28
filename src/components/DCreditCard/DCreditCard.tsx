a652fc3de464c6b67d0aff689be9c5bc0dd5647b
lnazarit <lnazarit@modyo.com>
2025-10-28 11:08:18 -0300

    feat: dmodal fix gap, and remove size props on creditcard component

--- src/components/DCreditCard/DCreditCard.tsx
+++ src/components/DCreditCard/DCreditCard.tsx
@@ -1,13 +1,12 @@
 import classNames from 'classnames';
 
 type Props = {
-  size?: 'sm' | 'md' | 'lg';
   className?: string;
-  cardBrand?: string;
+  brand?: string;
   isChipVisible?: boolean;
-  nameOnCard?: string;
-  cardNumber?: string;
-  cardHolderText?: string;
+  name?: string;
+  number?: string;
+  holderText?: string;
   isVertical?: boolean;
   logoImage?: string;
 };
@@ -15,19 +14,12 @@ type Props = {
 const DEFAULT_IMAGE = 'https://cdn.modyo.cloud/uploads/06b434f7-b943-4f54-9543-84a904e189aa/original/Visa_Logo_1_.png';
 const CHIP_IMAGE = 'https://cdn.modyo.cloud/uploads/4660ad00-e5d8-477e-8919-52b53d0a26fb/original/chip-debit-svgrepo-com_1_.png';
 
-const SIZE = {
-  sm: 'd-card-sm',
-  md: 'd-card-md',
-  lg: 'd-card-lg',
-};
-
 export default function DCreditCard(
   {
-    size = 'md',
-    cardBrand = 'visa',
-    nameOnCard,
-    cardNumber,
-    cardHolderText = 'Card Holder',
+    brand = 'visa',
+    name,
+    number,
+    holderText = 'Card Holder',
     logoImage,
     isChipVisible = true,
     className,
@@ -40,7 +32,6 @@ export default function DCreditCard(
         'd-credit-card overflow-hidden text-white',
         'position-relative rounded-3',
         'd-none d-lg-flex',
-        SIZE[size],
         isVertical && 'is-vertical',
         className,
       )}
@@ -48,7 +39,7 @@ export default function DCreditCard(
       <div className="d-credit-card-header">
         <img
           src={logoImage || DEFAULT_IMAGE}
-          alt={cardBrand}
+          alt={brand}
           className="d-credit-card-logo"
           width={100}
         />
@@ -66,12 +57,12 @@ export default function DCreditCard(
 
       <div className="d-credit-card-details mt-auto d-none d-sm-block">
         <div className="d-credit-card-number d-none d-sm-block mb-4">
-          {cardNumber}
+          {number}
         </div>
         <small className="d-block opacity-50">
-          {cardHolderText}
+          {holderText}
         </small>
-        <span className="name">{nameOnCard}</span>
+        <span className="name">{name}</span>
       </div>
     </div>
   );
