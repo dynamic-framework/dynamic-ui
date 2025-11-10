71e2c6bd2e7922e75695b9b7d97f214ea743f39f
Eduard Baron <42986303+efbarong@users.noreply.github.com>
2025-11-10 12:02:23 -0500

    docs: hide extra icon props and adjust some stories with initial default values (#893)
    * docs: enhance DButton story with additional argTypes and default args
    
    * docs: improve some stories
    
    * docs: adjust preview configuration
    
    * docs: remove aria controls

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,17 +2,55 @@ import type { Preview } from '@storybook/react-vite';
 
 const config: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on.*' },
+    layout: 'centered',
     docs: {
       source: {
         excludeDecorators: true,
         type: 'dynamic',
       },
-    },
-    layout: 'centered',
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
+      controls: {
+        matchers: {
+          color: /(background|color)$/i,
+          date: /Date$/,
+        },
+        expanded: true,
+        sort: 'requiredFirst',
+        exclude: [
+          'iconFamilyClass',
+          'iconFamilyPrefix',
+          'iconMaterialStyle',
+          'dataAttributes',
+          'iconCloseFamilyClass',
+          'iconCloseFamilyPrefix',
+          'iconCloseMaterialStyle',
+          'style',
+          'iconStartDisabled',
+          'iconStartFamilyClass',
+          'iconStartFamilyPrefix',
+          'iconStartMaterialStyle',
+          'iconStartAriaLabel',
+          'iconStartTabIndex',
+          'iconEndDisabled',
+          'iconEndFamilyClass',
+          'iconEndFamilyPrefix',
+          'iconEndMaterialStyle',
+          'iconEndAriaLabel',
+          'iconEndTabIndex',
+          'ariaLabel',
+          'loadingAriaLabel',
+          'ariaLabelledBy',
+          'ariaDescribedBy',
+          'ariaControls',
+          'ariaExpanded',
+          'ariaPressed',
+          'ariaHidden',
+          'ariaSelected',
+          'ariaDisabled',
+          'ariaChecked',
+          'ariaCurrent',
+          'ariaRole'
+        ],
       },
     },
     options: {
