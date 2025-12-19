05da6279cc62de45a51ea39e0c9206826ddd719d
Eduard Baron <ebaron@modyo.com>
2025-12-19 12:43:10 -0500

    fix: adjust storybook configuration and enhance QuickStart documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 import { dirname, join } from 'node:path';
 
-module.exports = {
+export default {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-controls'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-viewport'),
     getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
