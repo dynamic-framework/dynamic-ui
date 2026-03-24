4ca1998e33a53ef9127cadafc53211e2ef36c146
lnazarit <lnazarit@modyo.com>
2026-03-24 12:20:13 -0300

    chore: rollback develop

--- stories/components/DCarousel.stories.tsx
+++ stories/components/DCarousel.stories.tsx
@@ -19,7 +19,7 @@ To understand in more detail the aspects covered by this component, review the f
 + [Splide React Integration](https://splidejs.com/integration/react-splide/)
 
 ## CSS Variables
-| Variable                                         | className       | Type            | Description                   |
+| Variable                                         | Class       | Type            | Description                   |
 |--------------------------------------------------|-------------|-----------------|-------------------------------|
 | --${PREFIX_BS}carousel-arrow-space               | .d-carousel | css length unit | Arrow separation              |
 | --${PREFIX_BS}carousel-pagination-bottom         | .d-carousel | css length unit | Pagination bottom separation  |
@@ -160,61 +160,6 @@ export const WithBreakpoints: Story = {
       },
     },
   },
-};
-
-export const BaseBootstrap: Story = {
-  render: () => (
-    <div id="carouselExampleCaptions" className="carousel slide">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <svg aria-label="Placeholder: First slide" className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" height="400" preserveAspectRatio="xMidYMid slice" role="img" width="800" xmlns="http://www.w3.org/2000/svg">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-          </svg>
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <svg aria-label="Placeholder: First slide" className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" height="400" preserveAspectRatio="xMidYMid slice" role="img" width="800" xmlns="http://www.w3.org/2000/svg">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-          </svg>
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <svg aria-label="Placeholder: First slide" className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" height="400" preserveAspectRatio="xMidYMid slice" role="img" width="800" xmlns="http://www.w3.org/2000/svg">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#777" />
-            <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-          </svg>
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="visually-hidden">Next</span>
-      </button>
-      <div className="carousel-indicators mt-4">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" />
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" />
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" />
-      </div>
-    </div>
-  ),
 };
 
 export const AutoplayAndLoop: Story = {
